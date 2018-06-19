707d6874d02b0b8ba8d25fa03813f8dd53b53a99
ncying <ningchen@excelsia.io>
2018-06-19 18:48:45 +0800

    fix parameter style

--- src/main/scala/com/wavesplatform/state2/diffs/ContendSlotsTransactionDiff.scala
+++ src/main/scala/com/wavesplatform/state2/diffs/ContendSlotsTransactionDiff.scala
@@ -10,27 +10,21 @@ import scala.util.Right
 
 object ContendSlotsTransactionDiff {
   def apply(s: StateReader,fs: FunctionalitySettings,height: Int)(tx: ContendSlotsTransaction): Either[ValidationError, Diff] = {
-    val ap_gen = generatingBalance(s,fs,tx.sender,height)
-    val op_gen = s.slotAddress(tx.slotid) match {
+    val ContendGen = generatingBalance(s,fs,tx.sender,height)
+    val SlotGen = s.slotAddress(tx.slotid) match {
         //if the slot is occupied, return the generating balance, else return 0
       case Some(l) => Address.fromString(l).right.map( arr => generatingBalance(s,fs,arr,height)).getOrElse(0L)
       case None => 0L //here 0 can be changed to min contend cost
     }
-    //for test
-    //println(tx.slotid)
-    //println(tx.sender.toAddress.address)
-    //println(ap_gen)
-    //println(op_gen)
-    if (ap_gen > op_gen){
+    if (ContendGen > SlotGen){
       // charge transaction fee and contend the slot
-      //println("great")
       Right(Diff(height = height, tx = tx,
       portfolios = Map(tx.sender.toAddress -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
       slotids = Map(tx.slotid -> tx.sender.toAddress.address)))
     }
     else {
-      //Charge the transaction fee without any modification
-      //println("sorry")
+      // charge the transaction fee without any modification
+      // may return some warning
       Right(Diff(height = height, tx = tx,
         portfolios = Map(tx.sender.toAddress -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty))))
     }
