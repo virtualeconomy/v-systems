1094ac530fac392ec30e33ae2c6ed3985d7d05ec
ncying <ningchen@excelsia.io>
2018-06-19 18:52:45 +0800

    support releaseSlotsTx API

--- src/main/scala/scorex/api/http/spos/SPOSBroadcastApiRoute.scala
+++ src/main/scala/scorex/api/http/spos/SPOSBroadcastApiRoute.scala
@@ -18,7 +18,7 @@ case class SPOSBroadcastApiRoute(
     allChannels: ChannelGroup)
   extends ApiRoute with BroadcastRoute {
   override val route = pathPrefix("spos" / "broadcast") {
-    signedContend
+    signedContend ~ signRelease
   }
 
   @Path("/contend")
@@ -40,6 +40,28 @@ case class SPOSBroadcastApiRoute(
   def signedContend: Route = (path("contend") & post) {
     json[SignedContendSlotsRequest] { contendReq =>
       doBroadcast(contendReq.toTx)
+    }
+  }
+
+  @Path("/release")
+  @ApiOperation(value = "Broadcasts a signed release transaction",
+    httpMethod = "POST",
+    produces = "application/json",
+    consumes = "application/json")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(
+      name = "body",
+      value = "Json with data",
+      required = true,
+      paramType = "body",
+      dataType = "scorex.api.http.spos.SignedReleaseSlotsRequest",
+      defaultValue = "{\n\t\"slotid\": 0,\n\t\"senderPublicKey\": \"11111\",\n\t\"fee\": 100000\n\t\"timestamp\": 12345678,\n\t\"signature\": \"asdasdasd\"\n}"
+    )
+  ))
+  @ApiResponses(Array(new ApiResponse(code = 200, message = "Json with response or error")))
+  def signRelease: Route = (path("release") & post) {
+    json[SignedReleaseSlotsRequest] { releaseReq =>
+      doBroadcast(releaseReq.toTx)
     }
   }
 }
