461bca166acd35ca5667f3f4e446bac4cc28c29d
ncying <38449461+ncying@users.noreply.github.com>
2018-07-11 21:16:56 +0800

    fix unit test for weightedBalance (#31)
    * add weightedBalance in balanceSnapshots, add weighted/minting Balance calc fun
    
    * combine the weightedBalance calculation procedure in SPoSCalc.weightedBalanceCalc
    
    * fix snapshot issue in unit test

--- src/test/scala/com/wavesplatform/state2/reader/StateReaderEffectiveBalanceTest.scala
+++ src/test/scala/com/wavesplatform/state2/reader/StateReaderEffectiveBalanceTest.scala
@@ -21,17 +21,20 @@ class StateReaderEffectiveBalanceTest extends fixture.FunSuite with Matchers {
     test(storage)
   }
 
+  // ALL test here just set weightedBalance = 0
+  // TODO: test for weightedBalance
+
   test("exposes minimum of all 'current' and  one 'previous' of oldest record") { storage =>
-    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1))
-    storage.balanceSnapshots.put(accountIndexKey(acc, 75), (20, 0, 200))
-    storage.balanceSnapshots.put(accountIndexKey(acc, 90), (75, 0, 100))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1, 0))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 75), (20, 0, 200, 0))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 90), (75, 0, 100, 0))
     storage.lastBalanceSnapshotHeight.put(acc.bytes, 90)
 
     new StateReaderImpl(storage, new ReentrantReadWriteLock()).effectiveBalanceAtHeightWithConfirmations(acc, stateHeight, 50) shouldBe 1
   }
 
   test("exposes current effective balance if no records in past N blocks are made") { storage =>
-    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1, 0))
     storage.portfolios.put(acc.bytes, (1, (0, 0), Map.empty))
     storage.lastBalanceSnapshotHeight.put(acc.bytes, 20)
 
@@ -39,25 +42,25 @@ class StateReaderEffectiveBalanceTest extends fixture.FunSuite with Matchers {
   }
 
   test("doesn't include info older than N blocks") { storage =>
-    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1000))
-    storage.balanceSnapshots.put(accountIndexKey(acc, 50), (20, 0, 50000))
-    storage.balanceSnapshots.put(accountIndexKey(acc, 75), (50, 0, 100000))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1000, 0))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 50), (20, 0, 50000, 0))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 75), (50, 0, 100000, 0))
     storage.lastBalanceSnapshotHeight.put(acc.bytes, 75)
 
     new StateReaderImpl(storage, new ReentrantReadWriteLock()).effectiveBalanceAtHeightWithConfirmations(acc, stateHeight, 50) shouldBe 50000
   }
 
   test("includes most recent update") { storage =>
-    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1000))
-    storage.balanceSnapshots.put(accountIndexKey(acc, 51), (20, 0, 50000))
-    storage.balanceSnapshots.put(accountIndexKey(acc, 100), (51, 0, 1))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 20), (0, 0, 1000, 0))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 51), (20, 0, 50000, 0))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 100), (51, 0, 1, 0))
     storage.lastBalanceSnapshotHeight.put(acc.bytes, 100)
 
     new StateReaderImpl(storage, new ReentrantReadWriteLock()).effectiveBalanceAtHeightWithConfirmations(acc, stateHeight, 50) shouldBe 1
   }
 
   test("exposes zero if record was made in past N blocks") { storage =>
-    storage.balanceSnapshots.put(accountIndexKey(acc, 70), (0, 0, 1000))
+    storage.balanceSnapshots.put(accountIndexKey(acc, 70), (0, 0, 1000, 0))
     storage.lastBalanceSnapshotHeight.put(acc.bytes, 70)
     new StateReaderImpl(storage, new ReentrantReadWriteLock()).effectiveBalanceAtHeightWithConfirmations(acc, stateHeight, 50) shouldBe 0
   }
