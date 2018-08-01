736dddfc7e9c74b4afdc467d50664449913b558a
ncying <ningchen@excelsia.io>
2018-08-01 18:24:36 +0800

    add broadcast payment transaction

--- src/main/scala/vee/api/http/vee/PaymentApiRoute.scala
+++ src/main/scala/vee/api/http/vee/PaymentApiRoute.scala
@@ -7,14 +7,11 @@ import com.wavesplatform.settings.RestAPISettings
 import io.netty.channel.group.ChannelGroup
 import io.swagger.annotations._
 import scorex.BroadcastRoute
-import scorex.account.Address
 import scorex.api.http.assets.PaymentRequest
-import scorex.transaction.{PaymentTransaction, TransactionFactory}
+import scorex.transaction.TransactionFactory
 import scorex.utils.Time
 import vee.wallet.Wallet
 import scorex.api.http._
-import scorex.crypto.encode.Base58
-import scorex.waves.transaction.SignedPaymentRequest
 
 @Path("/vee")
 @Api(value = "/vee")
@@ -22,7 +19,7 @@ case class PaymentApiRoute(settings: RestAPISettings, wallet: Wallet, utx: UtxPo
   extends ApiRoute with BroadcastRoute {
 
   override lazy val route = pathPrefix("vee") {
-    payment~signPayment
+    payment~broadcastSignedPayment
   }
 
   @Path("/payment")
@@ -51,33 +48,25 @@ case class PaymentApiRoute(settings: RestAPISettings, wallet: Wallet, utx: UtxPo
   }
 
   @Path("/broadcast/payment")
-  @ApiOperation(value = "Create payment signed by address from wallet",
-    notes = "Create unique payment signed by address from wallet. Without broadcasting to network.",
-    httpMethod = "POST",
-    produces = "application/json",
-    consumes = "application/json")
+  @ApiOperation(value = "Broadcast signed payment.",
+    notes = "Publish signed payment to the Blockchain.",
+    httpMethod = "POST", produces = "application/json", consumes = "application/json")
   @ApiImplicitParams(Array(
     new ApiImplicitParam(
       name = "body",
       value = "Json with data",
       required = true,
       paramType = "body",
-      dataType = "scorex.api.http.assets.PaymentRequest",
-      defaultValue = "{\n\t\"amount\":400,\n\t\"fee\":1,\n\t\"sender\":\"senderId\",\n\t\"recipient\":\"recipientId\"\n}"
+      dataType = "vee.api.http.vee.SignedPaymentRequest",
+      defaultValue = "{\n\t\"timestamp\": 0,\n\t\"amount\":400,\n\t\"fee\":1,\n\t\"senderPublicKey\":\"senderPubKey\",\n\t\"senderAddress\":\"senderAddress\",\n\t\"recipient\":\"recipientId\",\n\t\"signature\":\"sig\"\n}"
     )
   ))
   @ApiResponses(Array(new ApiResponse(code = 200, message = "Json with response or error")))
-  def signPayment: Route = (post & path("broadcast" / "payment")) {
-    json[PaymentRequest] { payment =>
-      (for {
-        sender <- wallet.findPrivateKey(payment.sender)
-        recipient <- Address.fromString(payment.recipient)
-        pt <- PaymentTransaction.create(sender, recipient, payment.amount, payment.fee, time.correctedTime())
-      } yield pt)
-        .left.map(ApiError.fromValidationError)
-        .map { t =>
-          SignedPaymentRequest(t.timestamp, t.amount, t.fee, t.recipient.address, Base58.encode(t.sender.publicKey), t.sender.address, t.signature.base58)
-        }
+  def broadcastSignedPayment: Route = broadcastPaymentRoute("broadcast-payment")
+
+  private def broadcastPaymentRoute(suffix: String): Route = (path(suffix) & post) {
+    json[SignedPaymentRequest] { payment =>
+      doBroadcast(TransactionFactory.broadcastPayment(payment))
     }
   }
 }
