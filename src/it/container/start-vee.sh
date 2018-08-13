ea8a584ae9304683f69b99f8a07f39d10a733e96
zlvee <barde@excelsia.io>
2018-08-13 16:12:28 +0800

    fix varible in shell

--- src/it/container/start-vee.sh
+++ src/it/container/start-vee.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DEFAULT_NET_IP=`ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
-WAVES_NET_IP=`ifconfig eth1 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
+VEE_NET_IP=`ifconfig eth1 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
 
 echo Default: $DEFAULT_NET_IP
-echo Waves: $WAVES_NET_IP
+echo Vee: $VEE_NET_IP
 echo Options: $WAVES_OPTS
 
-#java -Dvee.network.declared-address=$WAVES_NET_IP:$WAVES_PORT $WAVES_OPTS -jar /opt/vee/vee.jar /opt/vee/template.conf
+#java -Dvee.network.declared-address=$VEE_NET_IP:$WAVES_PORT $WAVES_OPTS -jar /opt/vee/vee.jar /opt/vee/template.conf
 java -Dvee.network.declared-address=$DEFAULT_NET_IP:9923 $WAVES_OPTS -jar /opt/vee/vee.jar /opt/vee/template.conf
