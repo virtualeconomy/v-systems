6080fcbf7c1bcd01b0ec68414c93e68c366c6097
zlvee <barde@excelsia.io>
2018-08-24 15:18:08 +0800

    fix tx diff

--- src/main/scala/com/wavesplatform/state2/diffs/ContractTransactionDiff.scala
+++ src/main/scala/com/wavesplatform/state2/diffs/ContractTransactionDiff.scala
@@ -5,6 +5,7 @@ import com.wavesplatform.state2.{Diff, LeaseInfo, Portfolio}
 import scorex.transaction.ValidationError
 import scorex.transaction.ValidationError.GenericError
 import vee.transaction.contract.{ChangeContractStatusAction, ChangeContractStatusTransaction, CreateContractTransaction}
+import vee.transaction.proof.{EllipticCurve25519Proof, Proofs}
 
 import scala.util.{Left, Right}
 
@@ -12,11 +13,17 @@ object ContractTransactionDiff {
   def create(s: StateReader, height: Int)(tx: CreateContractTransaction): Either[ValidationError, Diff] = {
     //no need to validate the name duplication coz that will create a duplicate transacion and
     // will fail with duplicated transaction id
-    val contractInfo = (tx.contract.enabled, tx.sender.toAddress, tx.contract.content)
-    Right(Diff(height = height, tx = tx,
-      portfolios = Map(tx.sender.toAddress -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
-      contracts = Map(tx.contract.name -> contractInfo)
-    ))
+    val sender = EllipticCurve25519Proof.fromBytes(tx.proofs.proofs.head.bytes.arr).toOption.get.publicKey
+    if (tx.proofs.proofs.length > Proofs.MaxProofs){
+      Left(GenericError(s"Too many proofs, max ${Proofs.MaxProofs} proofs"))
+    }
+    else {
+      val contractInfo = (tx.contract.enabled, sender.toAddress, tx.contract.content)
+      Right(Diff(height = height, tx = tx,
+        portfolios = Map(sender.toAddress -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
+        contracts = Map(tx.contract.name -> contractInfo)
+      ))
+    }
   }
 
   def changeStatus(s: StateReader, height: Int)(tx: ChangeContractStatusTransaction): Either[ValidationError, Diff] = {
@@ -26,19 +33,23 @@ object ContractTransactionDiff {
     }
     for {
       contract <- contractInfo
+      proof <- EllipticCurve25519Proof.fromBytes(tx.proofs.proofs.head.bytes.arr)
+      sender = proof.publicKey
       originalAddr = contract._2
       originalEnabled = contract._1
       originalContent = contract._3
-      newContractInfo = (!originalEnabled, tx.sender.toAddress, originalContent)
-      validation <- if (!originalAddr.equals(tx.sender.toAddress.bytes)) {
+      newContractInfo = (!originalEnabled, sender.toAddress, originalContent)
+      validation <- if (!originalAddr.equals(sender.toAddress.bytes)) {
         Left(GenericError(s"Only the creater of the contract can change contract status"))
       } else if (originalEnabled && (tx.action == ChangeContractStatusAction.Enable)) {
         Left(GenericError(s"The contract already enabled"))
       } else if (!originalEnabled && (tx.action == ChangeContractStatusAction.Disable)) {
         Left(GenericError(s"The contract already disabled"))
-      } else Right(())
+      } else if (tx.proofs.proofs.length > Proofs.MaxProofs) {
+        Left(GenericError(s"Too many proofs, max ${Proofs.MaxProofs} proofs"))
+      }else Right(())
     } yield Diff(height = height, tx = tx,
-      portfolios = Map(tx.sender.toAddress -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
+      portfolios = Map(sender.toAddress -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
       contracts = Map(tx.contractName -> newContractInfo)
     )
   }
