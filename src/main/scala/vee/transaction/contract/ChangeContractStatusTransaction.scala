173cbdcef627162119610db993a0c726dd876406
zlvee <barde@excelsia.io>
2018-08-24 15:17:29 +0800

    change contranct tx to proven tx

--- src/main/scala/vee/transaction/contract/ChangeContractStatusTransaction.scala
+++ src/main/scala/vee/transaction/contract/ChangeContractStatusTransaction.scala
@@ -6,27 +6,26 @@ import com.google.common.primitives.{Bytes, Longs, Shorts}
 import com.wavesplatform.state2.ByteStr
 import play.api.libs.json.{JsObject, Json}
 import scorex.account.{PrivateKeyAccount, PublicKeyAccount}
-import scorex.crypto.EllipticCurveImpl
 import scorex.serialization.{BytesSerializable, Deser}
-import scorex.transaction.{AssetId, SignedTransaction, ValidationError}
-import scorex.transaction.TransactionParser.{KeyLength, TransactionType}
+import scorex.transaction.{AssetId, ValidationError}
+import scorex.transaction.TransactionParser.TransactionType
+import vee.transaction.proof.{EllipticCurve25519Proof, Proofs}
+import vee.transaction.ProvenTransaction
 
 import scala.util.{Failure, Success, Try}
 
-case class ChangeContractStatusTransaction private(sender: PublicKeyAccount,
-                                              contractName: String,
+case class ChangeContractStatusTransaction private(contractName: String,
                                               action: ChangeContractStatusAction.Value,
                                               fee: Long,
                                               feeScale: Short,
                                               timestamp: Long,
-                                              signature: ByteStr)
-  extends SignedTransaction {
+                                              proofs: Proofs)
+  extends ProvenTransaction {
 
   override val transactionType: TransactionType.Value = TransactionType.ChangeContractStatusTransaction
 
   lazy val toSign: Array[Byte] = Bytes.concat(
     Array(transactionType.id.toByte),
-    sender.publicKey,
     BytesSerializable.arrayWithSize(contractName.getBytes("UTF-8")),
     Array(action.id.toByte),
     Longs.toByteArray(fee),
@@ -44,40 +43,36 @@ case class ChangeContractStatusTransaction private(sender: PublicKeyAccount,
   // TODO
   // add feeScale in assetFee, need to change 100 later
   override val assetFee: (Option[AssetId], Long, Short) = (None, fee, 100)
-  override lazy val bytes: Array[Byte] = Bytes.concat(toSign, signature.arr)
+  override lazy val bytes: Array[Byte] = Bytes.concat(toSign, proofs.bytes)
 }
 
 object ChangeContractStatusTransaction {
 
   def parseTail(bytes: Array[Byte]): Try[ChangeContractStatusTransaction] = Try {
-    import EllipticCurveImpl._
-    val sender = PublicKeyAccount(bytes.slice(0, KeyLength))
-    val (contractBytes, contractEnd) = Deser.parseArraySize(bytes, KeyLength)
+    val (contractBytes, contractEnd) = Deser.parseArraySize(bytes, 0)
     val contractName = new String(contractBytes, "UTF-8")
     val action = ChangeContractStatusAction(bytes(contractEnd))
     val fee = Longs.fromByteArray(bytes.slice(contractEnd + 1, contractEnd + 9))
     val feeScale = Shorts.fromByteArray(bytes.slice(contractEnd + 9, contractEnd + 11))
     val timestamp = Longs.fromByteArray(bytes.slice(contractEnd + 11, contractEnd + 19))
-    val signature = ByteStr(bytes.slice(contractEnd + 19, contractEnd + 19 + SignatureLength))
-    val tx:Either[ValidationError, ChangeContractStatusTransaction] = for {
-      tx <- ChangeContractStatusTransaction.create(sender, contractName, action, fee, feeScale, timestamp, signature)
-    } yield tx
-    tx.fold(left => Failure(new Exception(left.toString)), right => Success(right))
+    (for {
+      proofs <- Proofs.fromBytes(bytes.slice(contractEnd + 19, bytes.length))
+      tx <- ChangeContractStatusTransaction.create(contractName, action, fee, feeScale, timestamp, proofs)
+    } yield tx).fold(left => Failure(new Exception(left.toString)), right => Success(right))
   }.flatten
 
-  def create(sender: PublicKeyAccount,
-             contractName: String,
+  def create(contractName: String,
              action: ChangeContractStatusAction.Value,
              fee: Long,
              feeScale: Short,
              timestamp: Long,
-             signature: ByteStr): Either[ValidationError, ChangeContractStatusTransaction] =
+             proofs: Proofs): Either[ValidationError, ChangeContractStatusTransaction] =
     if (fee <= 0) {
       Left(ValidationError.InsufficientFee)
     } else if (feeScale != 100) {
       Left(ValidationError.WrongFeeScale(feeScale))
     } else {
-      Right(ChangeContractStatusTransaction(sender, contractName, action, fee, feeScale, timestamp, signature))
+      Right(ChangeContractStatusTransaction(contractName, action, fee, feeScale, timestamp, proofs))
     }
 
   def create(sender: PrivateKeyAccount,
@@ -85,9 +80,20 @@ object ChangeContractStatusTransaction {
              action: ChangeContractStatusAction.Value,
              fee: Long,
              feeScale: Short,
-             timestamp: Long): Either[ValidationError, ChangeContractStatusTransaction] = {
-    create(sender, contractName, action, fee, feeScale, timestamp, ByteStr.empty).right.map { unsigned =>
-      unsigned.copy(signature = ByteStr(EllipticCurveImpl.sign(sender, unsigned.toSign)))
-    }
-  }
+             timestamp: Long): Either[ValidationError, ChangeContractStatusTransaction] = for {
+    unsigned <- create(contractName, action, fee, feeScale, timestamp, Proofs.empty)
+    proofs <- Proofs.create(List(EllipticCurve25519Proof.createProof(unsigned.toSign, sender).bytes))
+    tx <- create(contractName, action, fee, feeScale, timestamp, proofs)
+  } yield tx
+
+  def create(sender: PublicKeyAccount,
+             contractName: String,
+             action: ChangeContractStatusAction.Value,
+             fee: Long,
+             feeScale: Short,
+             timestamp: Long,
+             signature: ByteStr): Either[ValidationError, ChangeContractStatusTransaction] = for {
+    proofs <- Proofs.create(List(EllipticCurve25519Proof.buildProof(sender, signature).bytes))
+    tx <- create(contractName, action, fee, feeScale, timestamp, proofs)
+  } yield tx
 }
