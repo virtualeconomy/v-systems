7503a9265935e401bd15955d562042d2f06db677
zl730 <barde@excelsia.io>
2019-03-04 17:39:40 +0800

    change dir to vsys

---
+++ src/main/scala/vsys/db/LevelDBFactory.scala
@@ -0,0 +1,33 @@
+package vsys.db
+
+import org.iq80.leveldb.DBFactory
+import scorex.utils.ScorexLogging
+
+object LevelDBFactory extends ScorexLogging {
+  private val javaFactory   = "org.iq80.leveldb.impl.Iq80DBFactory"
+
+  lazy val factory: DBFactory = load
+
+  private def load: DBFactory = {
+    val loaders = Seq(ClassLoader.getSystemClassLoader, this.getClass.getClassLoader)
+
+    val names = Seq(javaFactory)
+
+    val pairs = names.flatMap(x => loaders.map(y => (x, y)))
+
+    pairs.view
+      .flatMap {
+        case (name, loader) =>
+          try {
+            val c = loader.loadClass(name).getConstructor().newInstance().asInstanceOf[DBFactory]
+            log.trace(s"Loaded ${c.getClass.getName} with $loader")
+            Some(c)
+          } catch {
+            case _: Throwable =>
+              None
+          }
+      }
+      .headOption
+      .getOrElse(throw new Exception(s"Could not load the factory classes: $javaFactory"))
+  }
+}
---
+++ src/main/scala/vsys/db/package.scala
@@ -0,0 +1,25 @@
+package vsys
+
+import java.io.File
+
+import org.iq80.leveldb.{DB, Options}
+import scorex.utils.ScorexLogging
+
+package object db extends ScorexLogging {
+
+  def openDB(path: String, recreate: Boolean = false): DB = {
+    log.debug(s"Open DB at $path")
+    val file = new File(path)
+    val options = new Options()
+      .createIfMissing(true)
+      .paranoidChecks(true)
+
+    if (recreate) {
+      LevelDBFactory.factory.destroy(file, options)
+    }
+
+    file.getParentFile.mkdirs()
+    LevelDBFactory.factory.open(file, options)
+  }
+
+}
