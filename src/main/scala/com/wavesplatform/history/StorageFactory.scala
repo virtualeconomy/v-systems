a42b991b40f9371ef325e172f9e062ce00c30bb5
zl730 <barde@excelsia.io>
2019-03-04 18:30:27 +0800

    update application and storage factory

--- src/main/scala/com/wavesplatform/history/StorageFactory.scala
+++ src/main/scala/com/wavesplatform/history/StorageFactory.scala
@@ -4,20 +4,20 @@ import java.util.concurrent.locks.{ReentrantReadWriteLock => RWL}
 
 import com.wavesplatform.settings.BlockchainSettings
 import com.wavesplatform.state2.reader.StateReader
-import com.wavesplatform.state2.{BlockchainUpdaterImpl, StateStorage, StateWriterImpl}
+import com.wavesplatform.state2.{BlockchainUpdaterImpl, StateStorage, StateWriterImpl, StateWriter}
 import scorex.transaction.{BlockchainUpdater, History}
 
 import org.iq80.leveldb.DB
 
 object StorageFactory {
 
-  private def createStateStorage(history: History, db:DB): StateStorage = {
+  private def createStateStorage(history: History, db: DB): StateStorage = {
     val ss = StateStorage(db, dropExisting = false)
     if (ss.getHeight <= history.height()) ss
     else StateStorage(db, dropExisting = true)
   }
 
-  def apply(db: DB, settings: BlockchainSettings): (History, AutoCloseable, StateReader, BlockchainUpdater) = {
+  def apply(db: DB, settings: BlockchainSettings): (History, StateWriter, StateReader, BlockchainUpdater) = {
     val lock = new RWL(true)
     val historyWriter = new HistoryWriterImpl(db, lock)
     val ss = createStateStorage(historyWriter, db)
