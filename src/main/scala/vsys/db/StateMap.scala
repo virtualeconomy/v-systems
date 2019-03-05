dd0437478fc25d76a3da7d38e9ddb0815b9d9bd7
zl730 <barde@excelsia.io>
2019-03-05 11:58:27 +0800

    remove useless comment

--- src/main/scala/vsys/db/StateMap.scala
+++ src/main/scala/vsys/db/StateMap.scala
@@ -1,8 +1,3 @@
-/*
- * Copyright 2004-2019 H2 Group. Multiple-Licensed under the MPL 2.0,
- * and the EPL 1.0 (http://h2database.com/html/license.html).
- * Initial Developer: H2 Group
- */
 package vsys.db
 
 import java.nio.ByteBuffer
