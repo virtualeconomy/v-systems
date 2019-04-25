75dac8f6f00c48f40cd05ecd28626ef9f6816f51
ncying <ningchen@v.systems>
2019-04-25 15:41:10 +0800

    change contract translator

--- src/test/scala/tools/ContractTranslator.scala
+++ src/test/scala/tools/ContractTranslator.scala
@@ -22,6 +22,8 @@ object ContractTranslator extends App {
   val bytes = ContractPermitted.contract.bytes.arr
 
   println(Base58.encode(bytes))
+  print("Contract Bytes Length:")
+  println(bytes.length)
 
   val languageCode = bytes.slice(0, LanguageCodeByteLength)
   val languageVersion = bytes.slice(LanguageCodeByteLength, LanguageCodeByteLength + LanguageVersionByteLength)
@@ -29,11 +31,13 @@ object ContractTranslator extends App {
   println("Language Code: " + convertBytesToHex(languageCode))
   println("Language Version: " + convertBytesToHex(languageVersion))
 
-  val (initializer, initializerEnd) = Deser.parseArraySize(bytes, LanguageCodeByteLength + LanguageVersionByteLength)
+  val (triggerBytes, triggerEnd) = Deser.parseArraySize(bytes, LanguageCodeByteLength + LanguageVersionByteLength)
+  val trigger = Deser.parseArrays(triggerBytes)
 
-  println("Initializer: " + convertBytesToHex(initializer))
+  println("Trigger:")
+  printSeqHex(trigger)
 
-  val (descriptorBytes, descriptorEnd) = Deser.parseArraySize(bytes, initializerEnd)
+  val (descriptorBytes, descriptorEnd) = Deser.parseArraySize(bytes, triggerEnd)
   val descriptor = Deser.parseArrays(descriptorBytes)
 
   println("Descriptor:")
@@ -46,41 +50,72 @@ object ContractTranslator extends App {
   printSeqHex(stateVar)
 
   val texture = Deser.parseArrays(bytes.slice(stateVarEnd, bytes.length))
-  val textureStr = textureFromBytes(texture)
+  val textualStr = textualFromBytes(texture)
 
   val dataTypeList = Seq("PublicKey", "Address", "Amount", "Int32", "ShortText", "ContractAccount", "Account")
 
-  printTexture(textureStr)
+  printTexture(textualStr)
 
-  printSeqFun(Seq(initializer), 0)
+  println("Triggers:")
+  printSeqFun(trigger, 0)
+  println("Descriptor Functions:")
   printSeqFun(descriptor, 1)
 
   def printFunction(bytes: Array[Byte], tp: Int): Unit = {
     opcFromBytes(bytes) match {
-      case Success((funIdx, retType, listParaTypes, listOpcLines)) =>
+      case Success((funcIdx, funcType, listReturnTypes, listParaTypes, listOpcLines)) =>
         if (listOpcLines.forall(_.length < 2)) {
           println("Function print fail. Invalid OPC lines.")
         } else {
-          val funString = if (tp == 0) textureStr.get._1 else textureStr.get._2
-          val idx = if (tp == 0) funIdx.toInt else funIdx.toInt - 1
-          val retTypeName = if (retType == 0) "void" else dataTypeList(retType - 1)
-          print(retTypeName + " function " + funString(idx)(0) + "(")
-          val nameList = funString(idx).tail
+          val ftString = if (tp == 0) textualStr.get._1 else textualStr.get._2
+          val idx = funcIdx.toInt
+          // TODO
+          // need a more complex ftTypes check
+          // print function or trigger type
+          val ftType = if (tp == 0) "OnInit Trigger" else "Public Function"
+          print(ftType + " ")
+
+          // print function or trigger name
+          val retNum = listReturnTypes.size
+          val ftName = ftString(idx)(retNum)
+          print(ftName + "(")
+
+          // print input variables
+          val inputVarList = ftString(idx).slice(retNum + 1, ftString(idx).length)
           List.range(0, listParaTypes.size).foreach { i =>
-            print(dataTypeList(listParaTypes(i) - 1) + " " + nameList(i))
+            print(dataTypeList(listParaTypes(i) - 1) + " " + inputVarList(i))
             if (i < listParaTypes.size - 1) print(", ")
           }
-          println(")")
+          print(")")
+
+          //print return variables
+          if (listReturnTypes.length == 1) {
+            print(" return ")
+            println(dataTypeList(listReturnTypes(0) - 1) + " " + ftString(idx)(0))
+          } else if (listReturnTypes.length > 1){
+            print(" return (")
+            List.range(0, retNum).foreach { i =>
+              print(dataTypeList(listReturnTypes(i) - 1) + " " + ftString(idx)(i))
+              if (i < listParaTypes.size - 1) print(", ")
+            }
+            println(")")
+          } else println()
+
+          // print function or trigger head opc
           print("| ")
-          print(convertBytesToHex(Shorts.toByteArray(funIdx)))
+          print(convertBytesToHex(Shorts.toByteArray(funcIdx)))
           print("| ")
-          print(convertBytesToHex(Array(retType)))
+          print(convertBytesToHex(Array(funcType)))
           print("| ")
           print(convertBytesToHex(listParaTypes))
+          print("| ")
+          print(convertBytesToHex(listReturnTypes))
           println("|")
+
+          // print opc lines
           listOpcLines.foreach { case l =>
             print("    ")
-            val x = opcToName(l, nameList)
+            val x = opcToName(l, inputVarList)
             println(x)
             print("    " + convertBytesToHex(l.slice(0, 2)))
             print("| ")
@@ -108,7 +143,7 @@ object ContractTranslator extends App {
 
   def printSeqHex(data: Seq[Array[Byte]]): Unit = {
     List.range(0, data.size).foreach { i =>
-      println("%02d".format(i + 1) + " | " + convertBytesToHex(data(i)))
+      println("%02d".format(i) + " | " + convertBytesToHex(data(i)))
     }
   }
 
@@ -128,12 +163,12 @@ object ContractTranslator extends App {
       printSeqSeqString(r._2)
       println("State Variables:")
       List.range(0, r._3.size).foreach { i =>
-        println("%02d".format(i + 1) + " | " + r._3(i))
+        println("%02d".format(i) + " | " + r._3(i))
       }
     }
   }
 
-  private def textureFromBytes(bs: Seq[Array[Byte]]): Try[(Seq[Seq[String]], Seq[Seq[String]], Seq[String])] = Try {
+  private def textualFromBytes(bs: Seq[Array[Byte]]): Try[(Seq[Seq[String]], Seq[Seq[String]], Seq[String])] = Try {
     val initializerFuncBytes = Deser.parseArrays(bs.head)
     val initializerFunc = funcFromBytes(initializerFuncBytes)
     val descriptorFuncBytes = Deser.parseArrays(bs(1))
@@ -146,9 +181,11 @@ object ContractTranslator extends App {
     bs.foldLeft(Seq.empty[Seq[String]]) { case (e, b) => {
       val (funcNameBytes, funcNameEnd) = Deser.parseArraySize(b, 0)
       val funcName = Deser.deserilizeString(funcNameBytes)
-      val listParaNameBytes = b.slice(funcNameEnd, b.length)
+      val (listReturnNameBytes, listReturnNameEnd) = Deser.parseArraySize(b, funcNameEnd)
+      val listReturnNames = paraFromBytes(listReturnNameBytes)
+      val listParaNameBytes = b.slice(listReturnNameEnd, b.length)
       val listParaNames = paraFromBytes(listParaNameBytes)
-      e :+ (funcName  +: listParaNames)
+      e :+ (listReturnNames ++ Seq(funcName) ++ listParaNames)
     }
     }
   }
@@ -162,20 +199,20 @@ object ContractTranslator extends App {
     }
   }
 
-  private def opcFromBytes(bytes: Array[Byte]): Try[(Short, Byte, Array[Byte], Seq[Array[Byte]])] = Try {
+  private def opcFromBytes(bytes: Array[Byte]): Try[(Short, Byte, Array[Byte], Array[Byte], Seq[Array[Byte]])] = Try {
     val funcIdx = Shorts.fromByteArray(bytes.slice(0, 2))
-    val (protoTypeBytes, protoTypeEnd) = Deser.parseArraySize(bytes, 2)
-    val returnType = protoTypeBytes.head
-    val listParaTypes = protoTypeBytes.tail
-    val (listOpcLinesBytes, _) = Deser.parseArraySize(bytes, protoTypeEnd)
+    val funcType = bytes(2)
+    val (listReturnTypes, listReturnTypeEnd) = Deser.parseArraySize(bytes, 3)
+    val (listParaTypes, listParaTypeEnd) = Deser.parseArraySize(bytes, listReturnTypeEnd)
+    val (listOpcLinesBytes, _) = Deser.parseArraySize(bytes, listParaTypeEnd)
     val listOpcLines = Deser.parseArrays(listOpcLinesBytes)
-    (funcIdx, returnType, listParaTypes, listOpcLines)
+    (funcIdx, funcType,listReturnTypes, listParaTypes, listOpcLines)
   }
 
   def opcToName(data: Array[Byte], nameList: Seq[String]): String = {
     val x = data(0)
     val y = data(1)
-    val stateNameList = textureStr.get._3
+    val stateNameList = textualStr.get._3
     x match {
 
       case opcType: Byte if opcType == OpcType.AssertOpc.id =>
@@ -185,59 +222,59 @@ object ContractTranslator extends App {
           case opcType: Byte if opcType == AssertType.LtInt64Assert.id => "opc_assert_ltInt64"
           case opcType: Byte if opcType == AssertType.GtZeroAssert.id => "opc_assert_gt0"
           case opcType: Byte if opcType == AssertType.EqAssert.id => "opc_assert_eq"
-          case opcType: Byte if opcType == AssertType.IsCallerOriginAssert.id => "opc_assert_caller " + nameList(data(2))
-          case opcType: Byte if opcType == AssertType.IsSignerOriginAssert.id => "opc_assert_singer " + nameList(data(2))
+          case opcType: Byte if opcType == AssertType.IsCallerOriginAssert.id => "opc_assert_caller(" + nameList(data(2)) + ")"
+          case opcType: Byte if opcType == AssertType.IsSignerOriginAssert.id => "opc_assert_singer(" + nameList(data(2)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.LoadOpc.id =>
         y match {
-          case opcType: Byte if opcType == LoadType.SignerLoad.id => "opc_load_env_signer " + nameList(data(2))
-          case opcType: Byte if opcType == LoadType.CallerLoad.id => "opc_load_env_caller " + nameList(data(2))
+          case opcType: Byte if opcType == LoadType.SignerLoad.id => nameList(data(2)) + " = opc_load_env_signer()"
+          case opcType: Byte if opcType == LoadType.CallerLoad.id => nameList(data(2)) + " = opc_load_env_caller()"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.CDBVOpc.id =>
         y match {
-          case opcType: Byte if opcType == CDBVType.SetCDBV.id => "opc_cdbv_set db." + stateNameList(data(2)) + " " + nameList(data(3))
+          case opcType: Byte if opcType == CDBVType.SetCDBV.id => "cdb." + stateNameList(data(2)) + " = opc_cdbv_set(" + nameList(data(3)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.CDBVROpc.id =>
         y match {
-          case opcType: Byte if opcType == CDBVRType.GetCDBVR.id => "opc_cdbvr_get db." + stateNameList(data(2)) + " " + nameList(data(3))
+          case opcType: Byte if opcType == CDBVRType.GetCDBVR.id => nameList(data(3)) + " = opc_cdbvr_get(cdb." + stateNameList(data(2)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.TDBOpc.id =>
         y match {
-          case opcType: Byte if opcType == TDBType.NewTokenTDB.id => "opc_tdb_new " + nameList(data(6)) + " " + nameList(data(7)) + " " + nameList(data(8))
-          case opcType: Byte if opcType == TDBType.SplitTDB.id => "opc_tdb_split " + nameList(data(3)) + " " + nameList(data(4))
+          case opcType: Byte if opcType == TDBType.NewTokenTDB.id => "opc_tdb_new(" + nameList(data(2)) + ", " + nameList(data(3)) + ", " + nameList(data(4)) + ")"
+          case opcType: Byte if opcType == TDBType.SplitTDB.id => "opc_tdb_split(" + nameList(data(2)) + ", " + nameList(data(3)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.TDBROpc.id =>
         y match {
-          case opcType: Byte if opcType == TDBRType.MaxTDBR.id => "opc_tdbr_get tdb." + stateNameList(data(2)) + " " + nameList(data(3)) + " "  + nameList(data(4))
-          case opcType: Byte if opcType == TDBRType.TotalTDBR.id => "opc_tdbr_total tdb." + stateNameList(data(2)) + " " + nameList(data(3)) + " "  + nameList(data(4))
+          case opcType: Byte if opcType == TDBRType.MaxTDBR.id => nameList(data(3)) + " = opc_tdbr_max(" + nameList(data(2)) + ")"
+          case opcType: Byte if opcType == TDBRType.TotalTDBR.id => nameList(data(3)) + " = opc_tdbr_total(" + nameList(data(2)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.TDBAOpc.id =>
         y match {
-          case opcType: Byte if opcType == TDBAType.DepositTDBA.id => "opc_tdba_deposit " + nameList(data(4)) + " " + nameList(data(5)) + " " + nameList(data(6))
-          case opcType: Byte if opcType == TDBAType.WithdrawTDBA.id => "opc_tdba_withdraw " + nameList(data(3)) + " " + nameList(data(4)) + " " + nameList(data(5))
-          case opcType: Byte if opcType == TDBAType.TransferTDBA.id => "opc_tdba_transfer " + nameList(data(2)) + " " + nameList(data(3)) + " " + nameList(data(4)) + " " + nameList(data(5))
+          case opcType: Byte if opcType == TDBAType.DepositTDBA.id => "opc_tdba_deposit(" + nameList(data(2)) + ", " + nameList(data(3)) + ", " + nameList(data(4)) + ")"
+          case opcType: Byte if opcType == TDBAType.WithdrawTDBA.id => "opc_tdba_withdraw(" + nameList(data(2)) + ", " + nameList(data(3)) + ", " + nameList(data(4)) + ")"
+          case opcType: Byte if opcType == TDBAType.TransferTDBA.id => "opc_tdba_transfer(" + nameList(data(2)) + ", " + nameList(data(3)) + ", " + nameList(data(4)) + ", " + nameList(data(5)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
       case opcType: Byte if opcType == OpcType.TDBAROpc.id =>
         y match {
-          case opcType: Byte if opcType == TDBARType.BalanceTBDAR.id => "opc_tdbar_balance " + nameList(data(2)) + " " + nameList(data(3)) + " " + nameList(data(4))
+          case opcType: Byte if opcType == TDBARType.BalanceTBDAR.id => nameList(data(4)) + " = opc_tdbar_balance(" + nameList(data(2)) + ", " + nameList(data(3)) + ")"
           case _ => "--- invalid opc code ---"
         }
 
-      case opcType: Byte if opcType == OpcType.ReturnOpc.id => "opc_return_value " + nameList(data(2))
+      case opcType: Byte if opcType == OpcType.ReturnOpc.id => "opc_value_return " + nameList(data(2))
 
       case _ => "--- invalid opc code ---"
 
