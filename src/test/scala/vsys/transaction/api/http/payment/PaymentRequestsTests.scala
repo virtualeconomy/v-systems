32ee967ca6921866d513bd156fd3c09fbb520e67
ncying <ningchen@v.systems>
2019-07-26 16:26:53 +0800

    add string amount to signed payment request

--- src/test/scala/vsys/transaction/api/http/payment/PaymentRequestsTests.scala
+++ src/test/scala/vsys/transaction/api/http/payment/PaymentRequestsTests.scala
@@ -63,4 +63,25 @@ class PaymentRequestsTests extends FunSuite with Matchers {
       "CRxqEuxhdZBEHX42MU4FfyJxuHmbDBTaHMhM3Uki7pLw", Option("v"), "4VPg4piLZGQz3vBqCPbjTfAR4cDErMi57rDvyith5XrQJDLryU2w2JsL3p4ejEqTPpctZ5YekpQwZPTtYiGo5yPC")
   }
 
+  test("SignedPaymentRequest with string amount") {
+    val json =
+      """
+        {
+         "senderPublicKey":"CRxqEuxhdZBEHX42MU4FfyJxuHmbDBTaHMhM3Uki7pLw",
+         "fee": 10000000,
+         "feeScale": 100,
+         "recipient": "ATuYeyAT3HBkMQkbZRoyjR75Ajxd1ppWBYV",
+         "amount": "100000000000000",
+         "attachment": "v",
+         "timestamp":0,
+         "signature":"4VPg4piLZGQz3vBqCPbjTfAR4cDErMi57rDvyith5XrQJDLryU2w2JsL3p4ejEqTPpctZ5YekpQwZPTtYiGo5yPC"
+         }
+      """
+
+    val req = Json.parse(json).validate[SignedPaymentRequest].get
+
+    req shouldBe SignedPaymentRequest(0L, 100000000000000L, 10000000L, 100, "ATuYeyAT3HBkMQkbZRoyjR75Ajxd1ppWBYV",
+      "CRxqEuxhdZBEHX42MU4FfyJxuHmbDBTaHMhM3Uki7pLw", Option("v"), "4VPg4piLZGQz3vBqCPbjTfAR4cDErMi57rDvyith5XrQJDLryU2w2JsL3p4ejEqTPpctZ5YekpQwZPTtYiGo5yPC")
+  }
+
 }
