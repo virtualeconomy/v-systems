68f730bada58ce0dbcf44abe96a36fd6ce819716
hvoid-build-block <hvoid@v.systems>
2019-08-20 16:50:06 +0800

    finish current funcs unit tests for webhook settings

---
+++ src/test/scala/vsys/settings/WebhookSettingsSpecification.scala
@@ -0,0 +1,262 @@
+package vsys.settings
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{FlatSpec, Matchers}
+
+class WebhookSettingsSpecification extends FlatSpec with Matchers {
+	"WebhookSettings" should "read Event values" in {
+		val config = loadConfig(ConfigFactory.parseString(
+			"""vsys {
+			  |	# Webhook Event Settings
+			  |  Event {
+			  |	    webHooks: [
+			  |	      {
+			  |	        url: "example.com",
+			  |	        secret: "secret_key",
+			  |	        encryptKey: "pubkey_for_encrypt",
+			  |	        events: [
+			  |	          {
+			  |	            type: 1, # Block Appended
+			  |	            rules: {
+			  |	              withTxs: false,
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0
+			  |	            }
+			  |	          },
+              |
+			  |	          {
+			  |	            type: 2, # Tx Confirmed
+			  |	            rules: {
+			  |	              relatedAccount: [
+			  |	                "addr1"
+			  |	              ],
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0,
+              |
+			  |	              includeTypes: [
+			  |	                1,
+			  |	                2
+			  |	              ],
+			  |
+			  |	              excludeTypes: [
+			  |	                3,
+			  |	                4
+			  |	              ],
+              |
+			  |	              amount: {
+			  |	                gte: 0,
+			  |	                gt: 0,
+			  |	                lte: 600000000000000000,
+			  |	                lt: 600000000000000000,
+			  |	                withFee: true
+			  |	              }
+			  |	            }
+			  |	          },
+              |
+			  |	          {
+			  |	            type: 3, # State Updated
+			  |	            rules: {
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0,
+              |
+			  |	              relatedAccount: [
+			  |	                "addr1"
+			  |	              ]
+			  |	            },
+			  |	          },
+              |
+			  |	          {
+			  |	            type: 4, # Block Rollback
+			  |	            rules: {
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0,
+              |
+			  |	              relatedAccount: [],
+              |
+			  |	              withTxsOfTypes: [
+			  |	                1,
+			  |		            2
+			  |	              ],
+              |
+			  |	              withTxsOfAccs: [
+			  |	                "addr3"
+			  |	              ],
+              |
+			  |	              withStateOfAccs: [
+			  |	                "addr4"
+			  |	              ]
+			  |	            }
+			  |	          }
+              |
+			  |	        ],
+			  |	        maxSize: 1000
+			  |	      }
+			  |	    ]
+			  |	  }
+			  |	}""".stripMargin))
+		val settings = WebhookSettings.fromConfig(config)(0)
+
+		settings.url should be("example.com")
+		settings.secretKey should be("secret_key")
+		settings.encryptKey should be("pubkey_for_encrypt")
+		settings.maxSize should be(1000)
+
+		settings.events map {aEvent =>
+			
+			aEvent match {
+				case e: BlockAppendedEventSettings =>
+					e.typeId should be(1) 
+					e.typeDescription should be("Block Appended")
+					e.withTxs should be(false)
+					e.afterHeight should be(0)
+					e.afterTime should be(0)
+					
+				case e: TxConfirmedEventSettings =>
+					e.typeId should be(2)
+					e.typeDescription should be("Tx Confirmed")
+					e.afterHeight should be(0)
+					e.afterTime should be(0)
+					e.relatedAcc should be(Seq("addr1"))
+					e.includeTypes should be(Seq(1, 2))
+					e.excludeTypes should be(Seq(3, 4))
+					e.amtGTE should be(0)
+					e.amtGT should be(0)
+					e.amtLTE should be(600000000000000000L)
+
+				case e: StateUpdatedEventSettings =>
+					e.typeId should be(3)
+					e.typeDescription should be("State Updated")
+					e.relatedAcc should be(Seq("addr1"))
+					e.afterHeight should be(0)
+					e.afterTime should be(0)
+
+				case e: BlockRollbackEventSettings => 
+					e.typeId should be(4)
+					e.typeDescription should be("Block Rollback")
+					e.relatedAcc should be(Seq.empty)
+					e.afterHeight should be(0)
+					e.afterTime should be(0)
+					e.withTxsOfTypes should be(Seq(1, 2))
+					e.withTxsOfAccs should be(Seq("addr3"))
+					e.withStateOfAccs should be(Seq("addr4"))
+
+
+				case _ => fail("The filter func in fromConfig is not working correctly")
+
+			}
+		}
+
+
+		val stringTypeConfig =  loadConfig(ConfigFactory.parseString(
+			"""vsys {
+			  |	# Webhook Event Settings
+			  |  Event {
+			  |	    webHooks: [
+			  |	      {
+			  |	        url: "example.com",
+			  |	        secret: "secret_key",
+			  |	        encryptKey: "pubkey_for_encrypt",
+			  |	        events: [
+			  |	          {
+			  |	            type: Block Appended,
+			  |	            rules: {
+			  |	              withTxs: false,
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0
+			  |	            }
+			  |	          },
+              |
+			  |	          {
+			  |	            type: Tx Confirmed,
+			  |	            rules: {
+			  |	              relatedAccount: [
+			  |	                "addr1"
+			  |	              ],
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0,
+              |
+			  |	              includeTypes: [
+			  |	                1,
+			  |	                2
+			  |	              ],
+			  |
+			  |	              excludeTypes: [
+			  |	                3,
+			  |	                4
+			  |	              ],
+              |
+			  |	              amount: {
+			  |	                gte: 0,
+			  |	                gt: 0,
+			  |	                lte: 600000000000000000,
+			  |	                lt: 600000000000000000,
+			  |	                withFee: true
+			  |	              }
+			  |	            }
+			  |	          },
+              |
+			  |	          {
+			  |	            type: State Updated,
+			  |	            rules: {
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0,
+              |
+			  |	              relatedAccount: [
+			  |	                "addr1"
+			  |	              ]
+			  |	            },
+			  |	          },
+              |
+			  |	          {
+			  |	            type: Block Rollback,
+			  |	            rules: {
+			  |	              afterHeight: 0,
+			  |	              afterTime: 0,
+              |
+			  |	              relatedAccount: [
+			  |	                "addr1"
+			  |	              ],
+              |
+			  |	              withTxsOfTypes: [
+			  |	                1,
+			  |		            2
+			  |	              ],
+              |
+			  |	              withTxsOfAccs: [
+			  |	                "addr3"
+			  |	              ],
+              |
+			  |	              withStateOfAccs: [
+			  |	                "addr4"
+			  |	              ]
+			  |	            }
+			  |	          },
+			  |
+			  |			  {
+			  |				type: 8
+			  |	        	rules: {
+			  |					afterHeight: 0,
+			  |					afterTime: 0
+			  |				}
+			  |			  }
+              |
+			  |	        ],
+			  |	        maxSize: 1000
+			  |	      }
+			  |	    ]
+			  |	  }
+			  |	}""".stripMargin))
+
+		val stringTypeSetting = WebhookSettings.fromConfig(stringTypeConfig)(0)
+
+		stringTypeSetting.events map {aEvent =>
+			aEvent match{
+				case e: BlockAppendedEventSettings => e.typeId should be(1)
+				case e: TxConfirmedEventSettings => e.typeId should be(2)
+				case e: StateUpdatedEventSettings => e.typeId should be(3)
+				case e: BlockRollbackEventSettings => e.typeId should be(4)
+				case _ => fail("The type description matching in setEventSettings goes wrong")
+			}
+		}
+	}
+}
