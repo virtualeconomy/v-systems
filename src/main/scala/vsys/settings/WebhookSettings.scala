ed200db6ca8c98fddb5e5bef42eef6f01812d529
hvoid-build-block <hvoid@v.systems>
2019-08-20 16:49:15 +0800

    clean unused import

--- src/main/scala/vsys/settings/WebhookSettings.scala
+++ src/main/scala/vsys/settings/WebhookSettings.scala
@@ -5,7 +5,6 @@ import com.typesafe.config.Config
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 import vsys.blockchain.transaction.ValidationError
-import vsys.utils.ScorexLogging
 
 //import scala.collection.JavaConversions._
 
@@ -16,7 +15,7 @@ case class WebhookSettings(url: String,
 						maxSize: Int = 1000
 						)
 
-object WebhookSettings extends ScorexLogging{
+object WebhookSettings {
 	val configPath = "vsys.Event"
 
 	def fromConfig(config: Config): Seq[WebhookSettings] = {
@@ -38,36 +37,40 @@ object WebhookSettings extends ScorexLogging{
 
 	private def setEventSettings(config: Config): Either[ValidationError, WebhookEventSettings] = {
 		val path = "rules"
+		val afterH = config.as[Long](s"$path.afterHeight")
+		val afterT = config.as[Long](s"$path.afterTime")
 
 		config.as[String]("type") match {
+
 			case "1" | "Block Appended" => 
-				Right(BlockAppendedEventSettings(config.as[Boolean](s"$path.withTxs"), 
-										   		 config.as[Long](s"$path.afterHeight"),
-										   		 config.as[Long](s"$path.afterTime")))
+				Right(BlockAppendedEventSettings(
+					config.as[Boolean](s"$path.withTxs"), 
+					afterH, afterT))
+
 			case "2" | "Tx Confirmed" =>
-				Right(TxConfirmedEventSettings(config.as[Seq[String]](s"$path.relatedAccount"),
-										 	   config.as[Long](s"$path.afterHeight"),
-											   config.as[Long](s"$path.afterTime"),
-											   config.as[Seq[Int]](s"$path.includeTypes"),
-											   config.as[Seq[Int]](s"$path.excludeTypes"),
-											   config.as[Long](s"$path.amount.gte"),
-											   config.as[Long](s"$path.amount.gt"),
-											   config.as[Long](s"$path.amount.lte"),
-											   config.as[Long](s"$path.amount.lt"),
-											   config.as[Boolean](s"$path.amount.withFee")) )
+				Right(TxConfirmedEventSettings(
+					config.as[Seq[String]](s"$path.relatedAccount"),
+			 	   	afterH, afterT,
+				   	config.as[Seq[Int]](s"$path.includeTypes"),
+				   	config.as[Seq[Int]](s"$path.excludeTypes"),
+				   	config.as[Long](s"$path.amount.gte"),
+				   	config.as[Long](s"$path.amount.gt"),
+				   	config.as[Long](s"$path.amount.lte"),
+				   	config.as[Long](s"$path.amount.lt"),
+				   	config.as[Boolean](s"$path.amount.withFee")))
 
 			case "3" | "State Updated" =>
-				Right(StateUpdatedEventSettings(config.as[Seq[String]](s"$path.relatedAccount"),
-											 	config.as[Long](s"$path.afterHeight"),
-											 	config.as[Long](s"$path.afterTime")))
+				Right(StateUpdatedEventSettings(
+					config.as[Seq[String]](s"$path.relatedAccount"), 
+					afterH, afterT))
 
 			case "4" | "Block Rollback" => 
-				Right(BlockRollbackEventSettings(config.as[Seq[String]](s"$path.relatedAccount"),
-											   	 config.as[Long](s"$path.afterHeight"),
-											   	 config.as[Long](s"$path.afterTime"),
-											   	 config.as[Seq[Int]](s"$path.withTxsOfTypes"),
-											   	 config.as[Seq[String]](s"$path.withTxsOfAccs"),
-											   	 config.as[Seq[String]](s"$path.withStateOfAccs")))
+				Right(BlockRollbackEventSettings(
+					config.as[Seq[String]](s"$path.relatedAccount"),
+				   	afterH,afterT,
+				   	config.as[Seq[Int]](s"$path.withTxsOfTypes"),
+				   	config.as[Seq[String]](s"$path.withTxsOfAccs"),
+				   	config.as[Seq[String]](s"$path.withStateOfAccs")))
 			case _ => 
 				Left(ValidationError.InvalidEventTypeError)
 			
