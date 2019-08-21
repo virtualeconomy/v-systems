3be632219b9bb4147b6b2bd0ae064b9d4ad03878
hvoid-build-block <hvoid@v.systems>
2019-08-21 16:29:02 +0800

    re-format the functions

--- src/main/scala/vsys/settings/WebhookEventSettings.scala
+++ src/main/scala/vsys/settings/WebhookEventSettings.scala
@@ -1,89 +1,98 @@
 package vsys.settings
 
 trait WebhookEventSettings{
-	val typeId: Int 
-	val typeDescription: String
+  val typeId: Int 
+  val typeDescription: String
 }
 
 case class BlockAppendedEventSettings(
-	withTxs: Boolean, 
-	afterHeight: Long, 
-	afterTime: Long
+  withTxs: Boolean, 
+  afterHeight: Long, 
+  afterTime: Long
 ) extends WebhookEventSettings {
-	override val typeId = 1
-	override val typeDescription = "Block Appended"
+  override val typeId = 1
+  override val typeDescription = "Block Appended"
 }
 
 case class TxConfirmedEventSettings(
-	relatedAcc: Seq[String],
-	afterHeight: Long,
-	afterTime: Long,
-	includeTypes: Seq[Int],
-	excludeTypes: Seq[Int],
-	amtGTE: Long, 			//amount greater or equal to 
-	amtGT: Long,  			//grater 
-	amtLTE: Long, 	//less than or equal to
-	amtLT: Long,
-	withFee: Boolean
+  relatedAcc: Seq[String],
+  afterHeight: Long,
+  afterTime: Long,
+  includeTypes: Seq[Int],
+  excludeTypes: Seq[Int],
+  amtGTE: Long,       //amount greater or equal to 
+  amtGT: Long,        //grater 
+  amtLTE: Long,       //less than or equal to
+  amtLT: Long,
+  withFee: Boolean
 ) extends WebhookEventSettings {
-	override val typeId = 2
-	override val typeDescription = "Tx Confirmed"
+  override val typeId = 2
+  override val typeDescription = "Tx Confirmed"
 }
 
 
 case class StateUpdatedEventSettings(
-	relatedAcc: Seq[String],
-	afterHeight: Long,
-	afterTime: Long
+  relatedAcc: Seq[String],
+  afterHeight: Long,
+  afterTime: Long
 ) extends WebhookEventSettings {
-	override val typeId = 3
-	override val typeDescription = "State Updated"
+  override val typeId = 3
+  override val typeDescription = "State Updated"
 }
 
 
 case class BlockRollbackEventSettings(
-	relatedAcc: Seq[String],
-	afterHeight: Long,
-	afterTime: Long,
-	withTxsOfTypes: Seq[Int],
-	withTxsOfAccs: Seq[String],
-	withStateOfAccs: Seq[String]
+  relatedAcc: Seq[String],
+  afterHeight: Long,
+  afterTime: Long,
+  withTxsOfTypes: Seq[Int],
+  withTxsOfAccs: Seq[String],
+  withStateOfAccs: Seq[String]
 ) extends WebhookEventSettings {
-	override val typeId = 4
-	override val typeDescription = "Block Rollback"
+  override val typeId = 4
+  override val typeDescription = "Block Rollback"
 }
 
 
 object BlockAppendedEventSettings {
 
-	def apply(withTxs: Boolean, afterHeight: Long, afterTime: Long): BlockAppendedEventSettings = {
-		new BlockAppendedEventSettings(withTxs, afterHeight, afterTime)
-	}
+  def apply(withTxs: Boolean, afterHeight: Long, afterTime: Long): BlockAppendedEventSettings = {
+    new BlockAppendedEventSettings(withTxs, afterHeight, afterTime)
+  }
 }
 
 object TxConfirmedEventSettings {
 
-	def apply(relatedAcc: Seq[String], afterHeight: Long, afterTime: Long,
-			  includeTypes: Seq[Int], excludeTypes: Seq[Int], amtGTE: Long, 
-			  amtGT: Long, amtLTE: Long, amtLT: Long, withFee: Boolean): TxConfirmedEventSettings = {
+  def apply(relatedAcc: Seq[String],
+            afterHeight: Long, 
+            afterTime: Long,
+            includeTypes: Seq[Int], 
+            excludeTypes: Seq[Int], 
+            amtGTE: Long, 
+            amtGT: Long, 
+            amtLTE: Long, 
+            amtLT: Long, 
+            withFee: Boolean): TxConfirmedEventSettings = {
 
-		new TxConfirmedEventSettings(relatedAcc, afterHeight, afterTime, includeTypes, 
-									 excludeTypes, amtGTE, amtGT, amtLTE, amtLT, withFee)
-	}
+    new TxConfirmedEventSettings(relatedAcc, afterHeight, afterTime, includeTypes, excludeTypes, amtGTE, amtGT, amtLTE, amtLT, withFee)
+  }
 }
 
 object StateUpdatedEventSettings {
 
-	def apply(relatedAcc: Seq[String], afterHeight: Long, afterTime: Long): StateUpdatedEventSettings = {
-		new StateUpdatedEventSettings(relatedAcc, afterHeight, afterTime)
-	}
+  def apply(relatedAcc: Seq[String], afterHeight: Long, afterTime: Long): StateUpdatedEventSettings = {
+    new StateUpdatedEventSettings(relatedAcc, afterHeight, afterTime)
+  }
 }
 
 object BlockRollbackEventSettings {
 
-	def apply(relatedAcc: Seq[String], afterHeight: Long, afterTime: Long,
-		      withTxsOfTypes: Seq[Int], withTxsOfAccs: Seq[String], withStateOfAccs: Seq[String]): BlockRollbackEventSettings = {
-		
-		new BlockRollbackEventSettings(relatedAcc, afterHeight, afterTime, withTxsOfTypes, withTxsOfAccs, withStateOfAccs)
-	}
+  def apply(relatedAcc: Seq[String], 
+            afterHeight: Long, 
+            afterTime: Long,
+            withTxsOfTypes: Seq[Int], 
+            withTxsOfAccs: Seq[String], 
+            withStateOfAccs: Seq[String]): BlockRollbackEventSettings = {
+    new BlockRollbackEventSettings(relatedAcc, afterHeight, afterTime, withTxsOfTypes, withTxsOfAccs, withStateOfAccs)
+  }
 }
--- src/main/scala/vsys/settings/WebhookSettings.scala
+++ src/main/scala/vsys/settings/WebhookSettings.scala
@@ -10,80 +10,77 @@ import vsys.utils.ScorexLogging
 //import scala.collection.JavaConversions._
 
 case class WebhookSettings(url: String,
-						events: Seq[WebhookEventSettings],
-						secretKey: String = "",
-						encryptKey: String = "",
-						maxSize: Int = 1000
-						)
+            events: Seq[WebhookEventSettings],
+            secretKey: String = "",
+            encryptKey: String = "",
+            maxSize: Int = 1000)
 
 object WebhookSettings extends ScorexLogging{
-	import vsys.utils.RichOptionalConfig._
-	val configPath = "vsys.Event"
+  import vsys.utils.RichOptionalConfig._
+  val configPath = "vsys.Event"
 
-	def fromConfig(config: Config): Seq[WebhookSettings] = {
-		val isEnable = config.getConfBoolean(s"$configPath.enable", false)
-		
-		if(!config.hasPath(s"$configPath.webHooks") && !isEnable){
-			Seq(WebhookSettings("0.0.0.0", null))
-		}else{
-			val hooks  = config.as[Seq[Config]](s"$configPath.webHooks") map {aHook => 
-				val url = aHook.as[String]("url")
-				val secret = aHook.getConfString("secret", "")
-				val encryptKey = aHook.getConfString("encryptKey", "")
-				val maxSize = aHook.getConfInt("maxSize", 1000)
-				
-				val eventList = aHook.as[Seq[Config]]("events").map(eventConfig =>
-					setEventSettings(eventConfig)
-				).filter(_.isRight).map(_.right.get)
+  def fromConfig(config: Config): Seq[WebhookSettings] = {
+    val isEnable = config.getConfBoolean(s"$configPath.enable", false)
+    
+    if(!config.hasPath(s"$configPath.webHooks") && !isEnable){
+      Seq(WebhookSettings("0.0.0.0", null))
+    }else{
+      val hooks  = config.as[Seq[Config]](s"$configPath.webHooks") map {aHook => 
+        val url = aHook.as[String]("url")
+        val secret = aHook.getConfString("secret", "")
+        val encryptKey = aHook.getConfString("encryptKey", "")
+        val maxSize = aHook.getConfInt("maxSize", 1000)
+        
+        val eventList = aHook.as[Seq[Config]]("events").map(eventConfig =>
+          setEventSettings(eventConfig)
+        ).filter(_.isRight).map(_.right.get)
 
-				(url, eventList, secret, encryptKey, maxSize)
+        (url, eventList, secret, encryptKey, maxSize)
 
-			}
-			hooks.map(h => WebhookSettings(h._1, h._2, h._3, h._4, h._5))
-		}
-	}
+      }
+      hooks.map(h => WebhookSettings(h._1, h._2, h._3, h._4, h._5))
+    }
+  }
 
-	private def setEventSettings(config: Config): Either[ValidationError, WebhookEventSettings] = {
-		val path = "rules"
-		val afterH = config.getConfInt(s"$path.afterHeight", 0)
-		val afterT = config.getConfInt(s"$path.afterTime", 0)
+  private def setEventSettings(config: Config): Either[ValidationError, WebhookEventSettings] = {
+    val path = "rules"
+    val afterH = config.getConfInt(s"$path.afterHeight", 0)
+    val afterT = config.getConfInt(s"$path.afterTime", 0)
 
-		config.as[String]("type") match {
+    config.as[String]("type") match {
 
-			case "1" | "Block Appended" => 
-				val withTxs = config.getConfBoolean(s"$path.withTxs", false)
-				Right(BlockAppendedEventSettings(withTxs, afterH, afterT))
+      case "1" | "Block Appended" => 
+        val withTxs = config.getConfBoolean(s"$path.withTxs", false)
+        Right(BlockAppendedEventSettings(withTxs, afterH, afterT))
 
-			case "2" | "Tx Confirmed" =>
-				val acc = config.getConfSeqString(s"$path.relatedAccount", null)
-				val inTypes = config.getConfSeqInt(s"$path.includeTypes", null)
-				val exTypes = config.getConfSeqInt(s"$path.excludeTypes", null)
-				val gteVal = config.getConfLong(s"$path.amount.gte", 0)
-				val gtVal = config.getConfLong(s"$path.amount.gt", 0)
-				val lteVal = config.getConfLong(s"$path.amount.lte", 6*10^9*10^8L)
-				val ltVal = config.getConfLong(s"$path.amount.lt", 6*10^9*10^8L)
-				val withFeeVal = config.getConfBoolean(s"$path.amount.withFee", false)
-				
-				Right(TxConfirmedEventSettings(acc, afterH, afterT, inTypes,
-				   							   exTypes, gteVal, gtVal, lteVal,
-											   ltVal, withFeeVal))
+      case "2" | "Tx Confirmed" =>
+        val acc = config.getConfSeqString(s"$path.relatedAccount", null)
+        val inTypes = config.getConfSeqInt(s"$path.includeTypes", null)
+        val exTypes = config.getConfSeqInt(s"$path.excludeTypes", null)
+        val gteVal = config.getConfLong(s"$path.amount.gte", 0)
+        val gtVal = config.getConfLong(s"$path.amount.gt", 0)
+        val lteVal = config.getConfLong(s"$path.amount.lte", 6*10^9*10^8L)
+        val ltVal = config.getConfLong(s"$path.amount.lt", 6*10^9*10^8L)
+        val withFeeVal = config.getConfBoolean(s"$path.amount.withFee", false)
+        
+        Right(TxConfirmedEventSettings(acc, afterH, afterT, inTypes, exTypes, gteVal, gtVal, lteVal, ltVal, withFeeVal))
 
-			case "3" | "State Updated" =>
-				val acc = config.getConfSeqString(s"$path.relatedAccount", null)
-				Right(StateUpdatedEventSettings(acc, afterH, afterT))
+      case "3" | "State Updated" =>
+        val acc = config.getConfSeqString(s"$path.relatedAccount", null)
+        Right(StateUpdatedEventSettings(acc, afterH, afterT))
 
-			case "4" | "Block Rollback" => 
-				val acc = config.getConfSeqString(s"$path.relatedAccount", null)
-				val wTxsOfTypes = config.getConfSeqInt(s"$path.withTxsOfTypes", null)
-				val wTxsOfAccs = config.getConfSeqString(s"$path.withTxsOfAccs", null)
-				val wStateOfAccs = config.getConfSeqString(s"$path.withStateOfAccs", null)
-				Right(BlockRollbackEventSettings(acc, afterH, afterT,
-											     wTxsOfTypes, wTxsOfAccs, wStateOfAccs))
-			case _ => 
-				Left(ValidationError.InvalidEventTypeError)
-			
-		}
-	}
+      case "4" | "Block Rollback" => 
+        val acc = config.getConfSeqString(s"$path.relatedAccount", null)
+        val wTxsOfTypes = config.getConfSeqInt(s"$path.withTxsOfTypes", null)
+        val wTxsOfAccs = config.getConfSeqString(s"$path.withTxsOfAccs", null)
+        val wStateOfAccs = config.getConfSeqString(s"$path.withStateOfAccs", null)
 
-	
+        Right(BlockRollbackEventSettings(acc, afterH, afterT, wTxsOfTypes, wTxsOfAccs, wStateOfAccs))
+      case _ => 
+        Left(ValidationError.InvalidEventTypeError)
+      
+    }
+  }
+
+  
 }
