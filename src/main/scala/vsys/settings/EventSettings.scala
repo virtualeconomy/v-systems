8327b796a6147f520d942cc0d54f6b28c98c812e
hvoid-build-block <hvoid@v.systems>
2019-08-22 14:27:01 +0800

    add EventSettings wrapper

--- src/main/scala/vsys/settings/EventSettings.scala
+++ src/main/scala/vsys/settings/EventSettings.scala
@@ -1,3 +1,23 @@
 package vsys.settings
 
+import com.typesafe.config.Config
+
+
 case class EventSettings(webhookSettings: Seq[WebhookSettings])
+
+
+object EventSettings {
+	import vsys.utils.RichOptionalConfig._
+
+	val configPath = "vsys.Event"
+
+	def fromConfig(config: Config): EventSettings = {
+		if(!config.getConfBoolean(s"$configPath.enable", false)){
+			//no sending any event notification
+			EventSettings(Seq.empty)
+		}else{
+			val webhookSettings = WebhookSettings.fromConfig(config)
+			EventSettings(webhookSettings)
+		}
+	}
+}
