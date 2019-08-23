18ae71fb0adf6e616bc772d78be97e8f00c12feb
hvoid-build-block <hvoid@v.systems>
2019-08-23 18:32:55 +0800

    update get option conf

--- src/main/scala/vsys/settings/EventSettings.scala
+++ src/main/scala/vsys/settings/EventSettings.scala
@@ -1,23 +1,22 @@
 package vsys.settings
 
 import com.typesafe.config.Config
-
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 
 case class EventSettings(webhookSettings: Seq[WebhookSettings])
 
 
 object EventSettings {
-	import vsys.utils.RichOptionalConfig._
+  val configPath = "vsys.Event"
 
-	val configPath = "vsys.Event"
-
-	def fromConfig(config: Config): EventSettings = {
-		if(!config.getConfBoolean(s"$configPath.enable", false)){
-			//no sending any event notification
-			EventSettings(Seq.empty)
-		}else{
-			val webhookSettings = WebhookSettings.fromConfig(config)
-			EventSettings(webhookSettings)
-		}
-	}
+  def fromConfig(config: Config): EventSettings = {
+    if(!config.as[Option[Boolean]](s"$configPath.enable").getOrElse(false)) {
+      //no sending any event notification
+      EventSettings(Seq.empty)
+    } else {
+      val webhookSettings = WebhookSettings.fromConfig(config)
+      EventSettings(webhookSettings)
+    }
+  }
 }
