dd82f56afcf2cfbeeca8d3a9f3c0179ec815224b
ncying <ningchen@v.systems>
2019-08-28 13:27:30 +0800

    add from call opc fun to ExecutionContext

--- src/main/scala/vsys/blockchain/contract/ExecutionContext.scala
+++ src/main/scala/vsys/blockchain/contract/ExecutionContext.scala
@@ -1,11 +1,12 @@
 package vsys.blockchain.contract
 
 import vsys.account.{ContractAccount, PublicKeyAccount}
-import vsys.blockchain.state.reader.StateReader
+import vsys.blockchain.state.reader.{CompositeStateReader, StateReader}
 import vsys.blockchain.transaction.{ProvenTransaction, ValidationError}
-import vsys.blockchain.transaction.ValidationError.{InvalidContractAddress, InvalidFunctionIndex}
+import vsys.blockchain.transaction.ValidationError.{GenericError, InvalidContractAddress, InvalidFunctionIndex}
 import vsys.blockchain.transaction.contract.{ExecuteContractFunctionTransaction, RegisterContractTransaction}
 import vsys.blockchain.transaction.proof.EllipticCurve25519Proof
+import vsys.blockchain.state.opcdiffs.OpcDiff
 import vsys.utils.serialization.Deser
 
 case class ExecutionContext(signers: Seq[PublicKeyAccount],
@@ -15,8 +16,9 @@ case class ExecutionContext(signers: Seq[PublicKeyAccount],
                             contractId: ContractAccount,
                             opcFunc: Array[Byte],
                             stateVar: Seq[Array[Byte]],
-                            description: Array[Byte]) {
-  
+                            description: Array[Byte],
+                            depth: Int) {
+
 }
 
 object ExecutionContext {
@@ -29,7 +31,7 @@ object ExecutionContext {
     val opcFunc = tx.contract.trigger.find(a => (a.length > 2) && (a(2) == 0.toByte)).getOrElse(Array[Byte]())
     val stateVar = tx.contract.stateVar
     val description = Deser.serilizeString(tx.description)
-    Right(ExecutionContext(signers, s, height, tx, contractId, opcFunc, stateVar, description))
+    Right(ExecutionContext(signers, s, height, tx, contractId, opcFunc, stateVar, description, 0))
   }
 
   def fromExeConTx(s: StateReader,
@@ -39,8 +41,39 @@ object ExecutionContext {
     val contractId = tx.contractId
     val description = tx.attachment
     s.contractContent(tx.contractId.bytes) match {
-      case Some(c) if tx.funcIdx >=0 && tx.funcIdx < c._3.descriptor.length => Right(ExecutionContext(signers, s, height, tx, contractId, c._3.descriptor(tx.funcIdx), c._3.stateVar, description))
+      case Some(c) if tx.funcIdx >=0 && tx.funcIdx < c._3.descriptor.length => Right(ExecutionContext(signers, s, height, tx, contractId, c._3.descriptor(tx.funcIdx), c._3.stateVar, description, 0))
       case Some(_) => Left(InvalidFunctionIndex)
+      case _ => Left(InvalidContractAddress)
+    }
+  }
+
+  def fromCallOpc(c: ExecutionContext,
+                  diff: OpcDiff,
+                  contractId: ContractAccount,
+                  callType: CallType.Value, // trigger or function
+                  callIndex: Int // trigger type or function index
+                 ): Either[ValidationError, ExecutionContext] = {
+    val state = new CompositeStateReader(c.state, diff.asBlockDiff(c.height, c.transaction))
+    if (c.depth == 1) {
+      Left(GenericError("stack overflow"))
+    }
+    state.contractContent(contractId.bytes) match {
+      case Some(contract) => if (callType == CallType.Trigger) {
+        val opcFunc = contract._3.trigger.find(a => (a.length > 2) && (a(2) == callIndex.toByte))
+        if (opcFunc.isDefined) {
+          Right(ExecutionContext(c.signers, state, c.height, c.transaction, contractId, opcFunc.get, contract._3.stateVar, c.description, c.depth + 1))
+        } else {
+          Left(GenericError("no such trigger"))
+        }
+      } else if (callType == CallType.Function) {
+        if (callIndex >= 0 && callIndex < contract._3.descriptor.length) {
+          Right(ExecutionContext(c.signers, state, c.height, c.transaction, contractId, contract._3.descriptor(callIndex), contract._3.stateVar, c.description, c.depth + 1))
+        } else {
+          Left(GenericError("invalid contract function index"))
+        }
+      } else {
+        Left(GenericError("invalid call type"))
+      }
       case _ => Left(InvalidContractAddress)
     }
   }
