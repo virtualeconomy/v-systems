bbb5dbc784b0b0c250673f8d3145200b2117269e
hvoid-build-block <hvoid@v.systems>
2019-08-28 17:52:01 +0800

    add safe conf reading and warning log to url reading in WebhookSettings

--- src/main/scala/vsys/settings/WebhookSettings.scala
+++ src/main/scala/vsys/settings/WebhookSettings.scala
@@ -6,6 +6,7 @@ import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 
 import vsys.utils.ScorexLogging
+import scala.util.{Try, Success, Failure}
 
 case class WebhookSettings(url: String,
             events: Seq[WebhookEventSettings],
@@ -20,28 +21,33 @@ object WebhookSettings extends ScorexLogging {
     
     if(!config.hasPath(s"$configPath.webHooks")) {
       //send all event notifications
-      Seq(WebhookSettings("0.0.0.0", null))
+      log.warn("Should Specify Webhook Config Under Event !!!")
+      Seq(WebhookSettings("0.0.0.0", Seq.empty))
 
     } else {
-      //config.getObject(s"$configPath.webHooks").asScala.foreach({case (k, v) => log.info(k.toString + v.toString)})
       val hooks  = config.as[Seq[Config]](s"$configPath.webHooks") map {aHook => 
-        val url = aHook.as[String]("url")
+        val url: String = Try(aHook.as[String]("url")) match {
+          case Success(s) => s
+          case Failure(e) => 
+            log.warn("Should Specify URL In Webhook Setting Under Event  !!!")
+            "0.0.0.0"
+        }
+
         val secret = aHook.as[Option[String]]("secret").getOrElse("")
         val encryptKey = aHook.as[Option[String]]("encryptKey").getOrElse("")
         val maxSize = aHook.as[Option[Int]]("maxSize").getOrElse(1000)
 
-        val eventList = aHook.as[Seq[Config]]("events").map(eventConfig => 
+        val eventList = aHook.as[Seq[Config]]("events").map(conf =>
             WebhookEventSettings(
-              eventConfig,
-              eventConfig.as[String]("type")
+              conf,
+              conf.as[String]("type")
             )
           ).filter(_.isRight).map(_.right.get)
 
-        
         (url, eventList, secret, encryptKey, maxSize)
-
       }
-      hooks.map(h => WebhookSettings(h._1, Seq.empty, h._3, h._4, h._5))
+
+      hooks.map(h => WebhookSettings(h._1, h._2, h._3, h._4, h._5))
     }
   }
 }
