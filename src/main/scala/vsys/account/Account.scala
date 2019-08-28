878ce1f0df3e52eae0212c583c1ace3ed8690c65
Icermli <haohan@excelsia.io>
2019-08-28 13:57:54 +0800

    add Account fromBytes

--- src/main/scala/vsys/account/Account.scala
+++ src/main/scala/vsys/account/Account.scala
@@ -1,6 +1,7 @@
 package vsys.account
 
 import vsys.blockchain.state.ByteStr
+import vsys.blockchain.transaction.ValidationError
 
 
 trait Account {
@@ -16,4 +17,21 @@ trait Account {
   }
 
   override def hashCode(): Int = java.util.Arrays.hashCode(bytes.arr)
+}
+
+object Account {
+
+  def fromBytes(bytes: Array[Byte], position: Int): Either[ValidationError, (Account, Int)] = {
+    bytes(position) match {
+      case Address.AddressVersion =>
+        val addressEnd = position + Address.AddressLength
+        val addressBytes = bytes.slice(position, addressEnd)
+        Address.fromBytes(addressBytes).map((_, addressEnd))
+      case ContractAccount.AddressVersion =>
+        val contractAccountEnd = position + ContractAccount.AddressLength
+        val contractAccountBytes = bytes.slice(position, contractAccountEnd)
+        ContractAccount.fromBytes(contractAccountBytes).map((_, contractAccountEnd))
+      case _ => Left(ValidationError.InvalidAddress)
+    }
+  }
 }
