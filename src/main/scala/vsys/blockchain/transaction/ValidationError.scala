d3bc0938882bfdbdf540668b19bc28aa6748e87b
zl730 <barde@v.systems>
2019-08-29 17:26:28 +0800

    refact contract validation error

--- src/main/scala/vsys/blockchain/transaction/ValidationError.scala
+++ src/main/scala/vsys/blockchain/transaction/ValidationError.scala
@@ -7,6 +7,12 @@ trait ValidationError
 
 object ValidationError {
 
+  import vsys.blockchain.transaction.TransactionStatus
+
+  trait ContractValidationError extends ValidationError {
+    val transactionStatus: TransactionStatus.Value
+  }
+
   case object InvalidAddress extends ValidationError
   case object InvalidDataType extends ValidationError
   case object InvalidDataLength extends ValidationError
@@ -26,26 +32,66 @@ object ValidationError {
   case object MissingSenderPrivateKey extends ValidationError
   case object UnsupportedTransactionType extends ValidationError
   case object InvalidRequestSignature extends ValidationError
-  case object InvalidContract extends ValidationError
-  case object InvalidContractAddress extends ValidationError
-  case object InvalidDataEntry extends ValidationError
-  case object InvalidFunctionIndex extends ValidationError
-  case object ContractDataTypeMismatch extends ValidationError
-  case object ContractInvalidStateVariable extends ValidationError
-  case object ContractStateVariableNotDefined extends ValidationError
-  case object ContractInvalidOPCData extends ValidationError
-  case object ContractUnsupportedOPC extends ValidationError
-  case object ContractInvalidSigner extends ValidationError
-  case object ContractInvalidCaller extends ValidationError
-  case object ContractInvalidFunction extends ValidationError
-  case object ContractInvalidTokenIndex extends ValidationError
-  case object ContractInvalidAmount extends ValidationError
-  case object ContractLocalVariableIndexOutOfRange extends ValidationError
-  case object ContractTokenBalanceInsufficient extends ValidationError
-  case object ContractTokenMaxExceeded extends ValidationError
-  case object ContractInvalidTokenInfo extends ValidationError
-  case object ContractUnsupportedWithdraw extends ValidationError
-  case object ContractUnsupportedDeposit extends ValidationError
+  case object InvalidContract extends ContractValidationError {
+    override val transactionStatus = TransactionStatus.InvalidContract
+  }
+  case object InvalidContractAddress extends ContractValidationError {
+    override val transactionStatus = TransactionStatus.InvalidContractAddress
+  }
+  case object InvalidDataEntry extends ValidationError {
+    override val transactionStatus = TransactionStatus.InvalidDataEntry
+  }
+  case object InvalidFunctionIndex extends ValidationError {
+    override val transactionStatus = TransactionStatus.InvalidFunctionIndex
+  }
+  case object ContractDataTypeMismatch extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractDataTypeMismatch
+  }
+  case object ContractInvalidStateVariable extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidStateVariable
+  }
+  case object ContractStateVariableNotDefined extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractStateVariableNotDefined
+  }
+  case object ContractInvalidOPCData extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidOPCData
+  }
+  case object ContractUnsupportedOPC extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractUnsupportedOPC
+  }
+  case object ContractInvalidSigner extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidSigner
+  }
+  case object ContractInvalidCaller extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidCaller
+  }
+  case object ContractInvalidFunction extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidFunction
+  }
+  case object ContractInvalidTokenIndex extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidTokenIndex
+  }
+  case object ContractInvalidAmount extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidAmount
+  }
+  case object ContractLocalVariableIndexOutOfRange extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractLocalVariableIndexOutOfRange
+  }
+  case object ContractTokenBalanceInsufficient extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractTokenBalanceInsufficient
+  }
+  case object ContractTokenMaxExceeded extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractTokenMaxExceeded
+  }
+  case object ContractInvalidTokenInfo extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractInvalidTokenInfo
+  }
+  case object ContractUnsupportedWithdraw extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractUnsupportedWithdraw
+  }
+  case object ContractUnsupportedDeposit extends ValidationError {
+    override val transactionStatus = TransactionStatus.ContractUnsupportedDeposit
+  }
   case class InvalidSignature(s: Signed, details: Option[InvalidSignature] = None) extends ValidationError
   case class OrderValidationError(order: Order, err: String) extends ValidationError
   case class AccountBalanceError(errs: Map[Address, String]) extends ValidationError
