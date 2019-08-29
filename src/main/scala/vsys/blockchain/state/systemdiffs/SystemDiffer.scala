5276deee635f29f5f6fdca8ad1a5b047517e6945
Icermli <haohan@excelsia.io>
2019-08-29 14:35:18 +0800

    update systemdiffer

--- src/main/scala/vsys/blockchain/state/systemdiffs/SystemDiffer.scala
+++ src/main/scala/vsys/blockchain/state/systemdiffs/SystemDiffer.scala
@@ -17,6 +17,21 @@ object SystemDiffer {
            (opc: Array[Byte],
             data: Seq[DataEntry]): Either[ValidationError, (OpcDiff, Seq[DataEntry])] = opc.head match {
 
+    case opcType: Byte if opcType == SystemType.SysAssert.id =>
+      SystemAssertDiff.parseBytes(context)(opc.tail, data) match {
+        case Right(opcDiff: OpcDiff) => Right((opcDiff, data))
+        case Left(validationError: ValidationError) => Left(validationError)
+      }
+    case opcType: Byte if opcType == SystemType.SysLoad.id =>
+      SystemLoadDiff.parseBytes(context)(opc.tail, data) match {
+        case Right(d: Seq[DataEntry]) => Right((OpcDiff.empty, d))
+        case Left(validationError: ValidationError) => Left(validationError)
+      }
+    case opcType: Byte if opcType == SystemType.SysTransfer.id =>
+      SystemTransferDiff.parseBytes(context)(opc.tail, data) match {
+        case Right(opcDiff: OpcDiff) => Right((opcDiff, data))
+        case Left(validationError: ValidationError) => Left(validationError)
+      }
     case _ => Left(ContractInvalidFunction)
   }
 }
--- src/main/scala/vsys/blockchain/state/systemdiffs/SystemTransferDiff.scala
+++ src/main/scala/vsys/blockchain/state/systemdiffs/SystemTransferDiff.scala
@@ -3,11 +3,29 @@ package vsys.blockchain.state.systemdiffs
 import vsys.blockchain.contract.{DataEntry, ExecutionContext}
 import vsys.blockchain.state.opcdiffs.OpcDiff
 import vsys.blockchain.transaction.ValidationError
+import vsys.blockchain.transaction.ValidationError.ContractInvalidOPCData
+
+import scala.util.Left
 
 object SystemTransferDiff {
 
   def transfer(context: ExecutionContext)
               (sender: DataEntry, recipient: DataEntry, amount: DataEntry): Either[ValidationError, OpcDiff] = {
     Right(OpcDiff.empty)
+  }
+
+  object TransferType extends Enumeration {
+    val Transfer = Value(1)
+  }
+
+  def parseBytes(context: ExecutionContext)
+                (bytes: Array[Byte], data: Seq[DataEntry]): Either[ValidationError, OpcDiff] = bytes.head match {
+    case opcType: Byte if opcType == TransferType.Transfer.id && checkInput(bytes,4, data.length) =>
+      transfer(context)(data(bytes(1)), data(bytes(2)), data(bytes(3)))
+    case _ => Left(ContractInvalidOPCData)
+  }
+
+  private def checkInput(bytes: Array[Byte], bLength: Int, dataLength: Int): Boolean = {
+    bytes.length == bLength && bytes.tail.max < dataLength && bytes.tail.min >= 0
   }
 }
