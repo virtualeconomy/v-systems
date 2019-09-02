7f6d374da7fdacae709de5765c3bf68118c05c01
zl730 <barde@v.systems>
2019-09-02 11:59:54 +0800

    fix format

--- src/main/scala/vsys/blockchain/state/diffs/ReleaseSlotsTransactionDiff.scala
+++ src/main/scala/vsys/blockchain/state/diffs/ReleaseSlotsTransactionDiff.scala
@@ -16,42 +16,34 @@ object ReleaseSlotsTransactionDiff {
       check the slot list, make sure it is not the last miner (set a min num of miner)
       set the min num to half of total num of slots
     */
-    tx.proofs.firstCurveProof.flatMap(proof => {
-      val senderAddr = proof.publicKey.toAddress
-
-      val MinimalSlotNumber = 10
-
-      val hasEnoughMiner = s.effectiveSlotAddressSize > MinimalSlotNumber
-
-      val isValidSlotID = tx.slotId < fs.numOfSlots && tx.slotId >=0 && (tx.slotId % SlotGap == 0)
-
-      val isValidAddress = s.slotAddress(tx.slotId) match {
+    val MinimalSlotNumber = 10
+    for {
+      proof <- tx.proofs.firstCurveProof
+      senderAddr = proof.publicKey.toAddress
+      // check if enough minter
+      _ <- if (s.effectiveSlotAddressSize <= MinimalSlotNumber) {
+        Left(GenericError(s"${s.effectiveSlotAddressSize} effective slot address(es) left, can not release the minting right"))
+      } else Right(())
+      // check if slotId valid
+      _ <- if (tx.slotId < fs.numOfSlots && tx.slotId >=0 && (tx.slotId % SlotGap == 0)) {
+        Right(())
+      } else Left(ValidationError.InvalidSlotId(tx.slotId))
+      isValidAddress = s.slotAddress(tx.slotId) match {
         case Some(l) if l == senderAddr.address => true
         case _ => false
       }
-
-      // add more ValidationError
-
-      if (hasEnoughMiner && isValidAddress && isValidSlotID) {
-        Right(Diff(
-          height = height,
-          tx = tx,
-          portfolios = Map(senderAddr -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
-          slotids = Map(tx.slotId -> None),
-          addToSlot = Map(senderAddr.address -> None),
-          slotNum = -1,
-          chargedFee = tx.fee
-        ))
-      }
-      else if (!isValidSlotID) {
-        Left(ValidationError.InvalidSlotId(tx.slotId))
-      }
-      else if (!hasEnoughMiner) {
-        Left(GenericError(s"${s.effectiveSlotAddressSize} effective slot address(es) left, can not release the minting right"))
-      }
-      else {
+      // check if address valid
+      _ <- if (isValidAddress) Right(()) else {
         Left(GenericError(s"${senderAddr.address} can not release the minting right of slot id: ${tx.slotId}"))
       }
-    })
+    } yield Diff(
+      height = height,
+      tx = tx,
+      portfolios = Map(senderAddr -> Portfolio(-tx.fee, LeaseInfo.empty, Map.empty)),
+      slotids = Map(tx.slotId -> None),
+      addToSlot = Map(senderAddr.address -> None),
+      slotNum = -1,
+      chargedFee = tx.fee
+    )
   }
 }
