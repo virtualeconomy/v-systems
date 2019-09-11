323cae46d7b528a43e62cbfbb5a01555c9533fda
hvoid-build-block <hvoid@v.systems>
2019-09-11 16:24:36 +0800

    add event types and simple actor

--- src/main/scala/vsys/events/EventTypes.scala
+++ src/main/scala/vsys/events/EventTypes.scala
@@ -1,4 +1,14 @@
 package vsys.events
 
+import vsys.account.Address
+import vsys.blockchain.transaction.ProcessedTransaction
+
 trait Event
-case class BlockAppendedEvent(url: String, secretKey: Option[String], encryptKey: Option[String], maxSize: Option[Int]) extends Event
+
+case class BlockAppendedEvent(
+	url: String,
+	secretKey: Option[String],
+	encryptKey: Option[String],
+	maxSize: Option[Int],
+	eventData: List[(Int, ProcessedTransaction, Set[Address])]
+) extends Event
--- src/main/scala/vsys/events/EventWriterActor.scala
+++ src/main/scala/vsys/events/EventWriterActor.scala
@@ -1,11 +1,16 @@
 package vsys.events
 
-import akka.actor.Props
-import akka.persistence._
+import akka.actor.Actor
 import vsys.utils.ScorexLogging
 
-class EventWriterActor() extends PersistentActor with ScorexLogging {
-  def receive: Receive = {
-    
+class EventWriterActor() extends Actor with ScorexLogging {
+
+  // TO DO: Should have MQ and protect memory usage in JVM
+  // It should handle more events
+  override def receive = {
+    case BlockAppendedEvent(url, secretKey, enKey, maxSize, eventData) =>
+      eventData.map {aList =>
+        aList._3.map(addr => log.info(addr.toString))
+      }
   }
 }
