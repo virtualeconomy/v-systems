2ef7ffd77d645aa86177750291c927c84d46202b
Icermli <haohan@excelsia.io>
2019-09-16 16:53:14 +0800

    modify system transfer diff

--- src/main/scala/vsys/blockchain/state/opcdiffs/SystemTransferDiff.scala
+++ src/main/scala/vsys/blockchain/state/opcdiffs/SystemTransferDiff.scala
@@ -1,7 +1,12 @@
 package vsys.blockchain.state.opcdiffs
 
-import vsys.blockchain.contract.{DataEntry, DataType, ExecutionContext}
-import vsys.blockchain.transaction.ValidationError
+import cats._
+import cats.implicits._
+import com.google.common.primitives.Longs
+import vsys.account.{Account, Address, ContractAccount}
+import vsys.blockchain.contract.{CallType, DataEntry, DataType, ExecutionContext}
+import vsys.blockchain.state.{LeaseInfo, Portfolio}
+import vsys.blockchain.transaction.{TransactionStatus, ValidationError}
 import vsys.blockchain.transaction.ValidationError.{ContractDataTypeMismatch, ContractInvalidOPCData}
 
 import scala.util.Left
@@ -10,6 +15,35 @@ object SystemTransferDiff {
 
   def transfer(context: ExecutionContext)
               (sender: DataEntry, recipient: DataEntry, amount: DataEntry): Either[ValidationError, OpcDiff] = {
+    lazy val feePortDiff: Map[Account, Portfolio] = toPortDiff(context.signers.head.toAddress, 0)
+    lazy val fromAccountPortDiff: Map[Account, Portfolio] = toPortDiff(Account.fromBytes(sender.data, 0).right.get._1, -Longs.fromByteArray(amount.data))
+    lazy val toAccountPortDiff: Map[Account, Portfolio] = toPortDiff(Account.fromBytes(recipient.data, 0).right.get._1, Longs.fromByteArray(amount.data))
+    lazy val portDiff = Monoid.combineAll(Seq(feePortDiff, fromAccountPortDiff, toAccountPortDiff))
+
+    def fromOpcFuncDiffer(df: OpcDiff, contractId: ContractAccount, callType: CallType.Value, callIndex: Int)
+                         (data: Seq[DataEntry]): Either[ValidationError, OpcDiff] = (for {
+      exContext <- ExecutionContext.fromCallOpc(context, df, contractId, callType, callIndex)
+      diff <- OpcFuncDiffer(exContext)(data)
+    } yield diff) match {
+      case Right(cdf) => Right(cdf.combine(OpcDiff(portfolios = portDiff)))
+      case Left(vd) => Left(vd)}
+
+    def fromOpcFuncDiffer2(df: OpcDiff, contractId: ContractAccount, callType: CallType.Value, callIndex: Int)
+                          (contractId2: ContractAccount, callType2: CallType.Value, callIndex2: Int)
+                          (data: Seq[DataEntry]): Either[ValidationError, OpcDiff] = (for {
+      exContext <- ExecutionContext.fromCallOpc(context, df, contractId, callType, callIndex)
+      diff <- OpcFuncDiffer(exContext)(data)
+    } yield diff) match {
+      case Right(cd) =>
+        val combineDiff = cd.combine(df)
+        (for {
+          exContext <- ExecutionContext.fromCallOpc(context, combineDiff, contractId2, callType2, callIndex2)
+          diff <- OpcFuncDiffer(exContext)(data)
+        } yield diff) match {
+          case Right(cdf) => Right(cdf.combine(cd).combine(OpcDiff(portfolios = portDiff)))
+          case Left(vd) => Left(vd)
+        }
+      case Left(vd) => Left(vd)}
 
     if (!checkDataType(sender, recipient)) {
       Left(ContractDataTypeMismatch)
@@ -17,12 +51,15 @@ object SystemTransferDiff {
       sender.dataType match {
         case DataType.Address if context.signers.head.toAddress.bytes.arr sameElements sender.bytes =>
           recipient.dataType match {
-            case DataType.Address => Right(OpcDiff.empty)
-            case _ => Right(OpcDiff.empty)
+            case DataType.Address => Right(OpcDiff(portfolios = portDiff))
+            case _ => fromOpcFuncDiffer(OpcDiff(portfolios = feePortDiff.combine(fromAccountPortDiff)),
+              ContractAccount.fromBytes(recipient.data).right.get, CallType.Trigger, 1)(Seq(sender, recipient, amount))
           }
         case _ => recipient.dataType match {
-          case DataType.Address => Right(OpcDiff.empty)
-          case _ => Right(OpcDiff.empty)
+          case DataType.Address => fromOpcFuncDiffer(OpcDiff(portfolios = feePortDiff),
+            ContractAccount.fromBytes(sender.data).right.get, CallType.Trigger, 2)(Seq(sender, recipient, amount))
+          case _ => fromOpcFuncDiffer2(OpcDiff(portfolios = feePortDiff), ContractAccount.fromBytes(sender.data).right.get,
+            CallType.Trigger, 2)(ContractAccount.fromBytes(recipient.data).right.get, CallType.Trigger, 1)(Seq(sender, recipient, amount))
         }
       }
     }
@@ -47,4 +84,12 @@ object SystemTransferDiff {
     (sender.dataType == DataType.Address || sender.dataType == DataType.ContractAccount) &&
       (recipient.dataType == DataType.Address && recipient.dataType == DataType.ContractAccount)
   }
+
+  private def toPortDiff(account: Account,
+                         amount: Long): Map[Account, Portfolio] = Map(
+    account -> Portfolio(
+      balance = amount,
+      LeaseInfo.empty,
+      assets = Map.empty))
+
 }
