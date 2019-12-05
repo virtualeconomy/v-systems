0a59f44fb8a44da90a0a5121423bc48bc727d729
ncying <ningchen@v.systems>
2019-12-05 12:55:02 +0800

    add basic deposit withdraw contract tests

---
+++ src/test/scala/vsys/blockchain/contract/basic/BasicContractGen.scala
@@ -0,0 +1,32 @@
+package vsys.blockchain.contract.basic
+
+import org.scalacheck.Gen
+import vsys.account.PrivateKeyAccount
+import vsys.blockchain.contract.{Contract, ContractDepositWithdrawProductive, ContractDepositWithdraw, DataEntry, DataType}
+import vsys.blockchain.contract.ContractGenHelper._
+import vsys.blockchain.state._
+import vsys.blockchain.transaction.GenesisTransaction
+import vsys.blockchain.transaction.contract.RegisterContractTransaction
+
+trait BasicContractGen {
+
+  def contractGen(Productive: Boolean): Gen[Contract] =
+    if (Productive) ContractDepositWithdrawProductive.contract
+    else ContractDepositWithdraw.contract
+
+  def initDepositWithdrawDataStackGen(tokenId: Array[Byte]): Gen[Seq[DataEntry]] = for {
+    tokenId <- Gen.const(DataEntry(tokenId, DataType.TokenId))
+  } yield Seq(tokenId)
+
+  def genesisBasicGen(rep: PrivateKeyAccount, ts: Long): Gen[GenesisTransaction] =
+    GenesisTransaction.create(rep, ENOUGH_AMT, -1, ts).explicitGet()
+
+  def registerDepositWithdrawContractGen(signer: PrivateKeyAccount, contract: Contract, description: String,
+                                         fee: Long, ts: Long): Gen[RegisterContractTransaction] =
+    RegisterContractTransaction.create(signer, contract, Seq(), description, fee, feeScale, ts).explicitGet()
+
+  def registerDepositWithdrawProductiveContractGen(signer: PrivateKeyAccount, contract: Contract, dataStack: Seq[DataEntry],
+                                                   description: String, fee: Long, ts: Long): Gen[RegisterContractTransaction] =
+    RegisterContractTransaction.create(signer, contract, dataStack, description, fee, feeScale, ts).explicitGet()
+
+}
