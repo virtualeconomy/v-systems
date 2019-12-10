12f7b890e3a1655e4387fa3a6e8c03d254214950
ncying <ningchen@v.systems>
2019-12-10 14:33:12 +0800

    resolve option code style issues in api test

--- src/test/scala/vsys/api/http/payment/PaymentRouteSpec.scala
+++ src/test/scala/vsys/api/http/payment/PaymentRouteSpec.scala
@@ -8,6 +8,8 @@ import play.api.libs.json.{JsObject, Json}
 import scorex.crypto.encode.Base58
 import vsys.api.http.ApiMarshallers._
 import vsys.api.http._
+import vsys.account.PrivateKeyAccount
+import vsys.blockchain.state._
 import vsys.blockchain.UtxPool
 import vsys.blockchain.transaction.{PaymentTransaction, Transaction, TransactionGen}
 import vsys.utils.Time
@@ -23,14 +25,14 @@ class PaymentRouteSpec extends RouteSpec("/vsys/payment")
 
   "accepts payments" in {
     forAll(accountGen.label("recipient"), positiveLongGen.label("amount"), smallFeeGen.label("fee"), feeScaleGen.label("feeScale"), attachmentGen.label("attachment")) {
-      case (recipient, amount, fee, feeScale, attachment) =>
+      case (recipient: PrivateKeyAccount, amount: Long, fee: Long, feeScale, attachment: Array[Byte]) =>
 
         val timestamp = System.currentTimeMillis * 1000000L
         val time = mock[Time]
         (time.getTimestamp _).expects().returns(timestamp).anyNumberOfTimes()
 
-        val sender = testWallet.privateKeyAccounts.head
-        val tx = PaymentTransaction.create(sender, recipient, amount, fee, feeScale, timestamp, attachment)
+        val sender = testWallet.privateKeyAccounts(0)
+        val tx = PaymentTransaction.create(sender, recipient, amount, fee, feeScale, timestamp, attachment).explicitGet()
 
         val route = PaymentApiRoute(restAPISettings, testWallet, utx, allChannels, time).route
 
@@ -40,15 +42,15 @@ class PaymentRouteSpec extends RouteSpec("/vsys/payment")
         Post(routePath(""), req) ~> api_key(apiKey) ~> route ~> check {
           val resp = responseAs[JsObject]
 
-          (resp \ "id").as[String] shouldEqual tx.right.get.id.toString
+          (resp \ "id").as[String] shouldEqual tx.id.toString
           (resp \ "assetId").asOpt[String] shouldEqual None
           (resp \ "feeAsset").asOpt[String] shouldEqual None
           (resp \ "type").as[Int] shouldEqual 2
           (resp \ "fee").as[Long] shouldEqual fee
           (resp \ "feeScale").as[Short] shouldEqual 100
           (resp \ "amount").as[Long] shouldEqual amount
-          (resp \ "timestamp").as[Long] shouldEqual tx.right.get.timestamp
-          (resp \ "attachment").as[String] shouldEqual Base58.encode(tx.right.get.attachment)
+          (resp \ "timestamp").as[Long] shouldEqual tx.timestamp
+          (resp \ "attachment").as[String] shouldEqual Base58.encode(tx.attachment)
           (resp \ "recipient").as[String] shouldEqual recipient.stringRepr
         }
     }
