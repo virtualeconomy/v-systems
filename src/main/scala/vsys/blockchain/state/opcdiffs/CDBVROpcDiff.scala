0a1d3d3ac2fe9984f37043c38ce81114090a5c21
Icermli <haohan@excelsia.io>
2019-12-24 11:57:38 +0800

    add constantGet opc

--- src/main/scala/vsys/blockchain/state/opcdiffs/CDBVROpcDiff.scala
+++ src/main/scala/vsys/blockchain/state/opcdiffs/CDBVROpcDiff.scala
@@ -25,6 +25,14 @@ object CDBVROpcDiff extends OpcDiffer {
     }
   }
 
+  def constantGet(context: ExecutionContext)(constant: Array[Byte], dataStack: Seq[DataEntry],
+                                             pointer: Byte): Either[ValidationError, Seq[DataEntry]] = {
+    DataEntry.fromBytes(constant) match {
+      case Right(v) => Right(dataStack.patch(pointer, Seq(v), 1))
+      case Left(e) => Left(e)
+    }
+  }
+
   def mapGet(context: ExecutionContext)(stateMap: Array[Byte], keyValue: DataEntry, dataStack: Seq[DataEntry],
                                         pointer: Byte): Either[ValidationError, Seq[DataEntry]] = {
     if (!checkStateMap(stateMap, keyValue.dataType, DataType(stateMap(2)))) {
@@ -68,7 +76,7 @@ object CDBVROpcDiff extends OpcDiffer {
   }
 
   object CDBVRType extends Enumeration(1) {
-    val GetCDBVR, MapGetOrDefaultCDBVR = Value
+    val GetCDBVR, MapGetOrDefaultCDBVR, ConstantGetCDBVR= Value
   }
 
   private def checkCDBVRIndex(bytes: Array[Byte], id: Int, stateVarOrMapSize: Int): Boolean =
@@ -80,6 +88,7 @@ object CDBVROpcDiff extends OpcDiffer {
         get(context)(context.stateVar(bytes(1)), data, bytes(2))
       case (Some(CDBVRType.MapGetOrDefaultCDBVR), 4) if checkCDBVRIndex(bytes, 1, context.stateMap.length) && bytes(2) >=0 &&
         bytes(2) <= data.length => mapGetOrDefault(context)(context.stateMap(bytes(1)), data(bytes(2)), data, bytes(3))
+      case (Some(CDBVRType.ConstantGetCDBVR), l) if l > 2 => constantGet(context)(bytes.slice(1, l-1), data, bytes(l-1))
       case _ => Left(ContractInvalidOPCData)
     }
 }
