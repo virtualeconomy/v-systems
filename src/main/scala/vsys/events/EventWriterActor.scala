dc8f04c7f16670c88167ec3542c4a85ffddc30b5
hvoid-build-block <hvoid@v.systems>
2019-12-25 17:34:27 +0800

    simplify event writer actor

--- src/main/scala/vsys/events/EventWriterActor.scala
+++ src/main/scala/vsys/events/EventWriterActor.scala
@@ -1,6 +1,6 @@
 package vsys.events
 
-import akka.actor.{Actor, ActorRef, Terminated}
+import akka.actor.{Actor, ActorRef}
 import vsys.utils.{ScorexLogging, SimpleEventQueue}
 
 case class EventWriterActor(queue: SimpleEventQueue, dispatcher: ActorRef) extends Actor with ScorexLogging {
@@ -9,19 +9,7 @@ case class EventWriterActor(queue: SimpleEventQueue, dispatcher: ActorRef) exten
   // TO DO: Should have MQ and protect memory usage in JVM
   // It should handle more events
   def receive = {
-    case e: BlockAppendedEvent =>
-      queue.enqueue(e)
-      dispatcher ! Tuple2(queue, e.maxSize)
-
-    case e: TxConfirmedEvent =>
-      queue.enqueue(e)
-      dispatcher ! Tuple2(queue, e.maxSize)
-
-    case e: StateUpdatedEvent =>
-      queue.enqueue(e)
-      dispatcher ! Tuple2(queue, e.maxSize)
-      
-    case e: BlockRollbackEvent =>
+    case e: Event =>
       queue.enqueue(e)
       dispatcher ! Tuple2(queue, e.maxSize)
   }
