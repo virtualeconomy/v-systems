c8ba8c76ef1b6f59500cc5d031674c4b038df8a1
hvoid-build-block <hvoid@v.systems>
2019-12-25 17:33:46 +0800

    refactor event types and func data struct

--- src/main/scala/vsys/blockchain/contract/FuncDataStruct.scala
+++ src/main/scala/vsys/blockchain/contract/FuncDataStruct.scala
@@ -2,9 +2,11 @@ package vsys.blockchain.contract
 
 trait FuncDataStruct {
   val functionIndex: Int
+  val funcName: String
   val signer: String
   val contractId: String
   val tokenId: String
+  val transactionFee: Long
 }
 
 trait FuncAmtInvolved {
@@ -16,70 +18,94 @@ object FuncAmtInvolved {}
 
 case class SupercedeFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   val newIssuer: String
-) extends FuncDataStruct
+) extends FuncDataStruct {
+  override val funcName = "supercede"
+}
 
 case class IssueFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   override val amount: Long
-) extends FuncDataStruct with FuncAmtInvolved
+) extends FuncDataStruct with FuncAmtInvolved {
+  override val funcName = "issue"
+}
 
 case class DestroyFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   override val amount: Long
-) extends FuncDataStruct with FuncAmtInvolved
+) extends FuncDataStruct with FuncAmtInvolved {
+  override val funcName = "destroy"
+}
 
 case class SplitFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   val unity: Long
-) extends FuncDataStruct
+) extends FuncDataStruct {
+  override val funcName = "split"
+}
 
 case class SendFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   val recipient: String,
   override val amount: Long
-) extends FuncDataStruct with FuncAmtInvolved
+) extends FuncDataStruct with FuncAmtInvolved {
+  override val funcName = "send"
+}
 
 // TO DO: The following case classes need to be verified
 case class TransferFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   val sender: String,
   val recipient: String,
   override val amount: Long,
-) extends FuncDataStruct with FuncAmtInvolved
+) extends FuncDataStruct with FuncAmtInvolved {
+  override val funcName = "transfer"
+}
 
 case class DepositFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   val sender: String,
   override val amount: Long
-) extends FuncDataStruct with FuncAmtInvolved
+) extends FuncDataStruct with FuncAmtInvolved {
+  override val funcName = "deposit"
+}
 
 case class WithdrawFuncData(
   override val functionIndex: Int,
+  override val transactionFee: Long,
   override val signer: String,
   override val contractId: String,
   override val tokenId: String,
   val recipient: String,
   override val amount: Long
-) extends FuncDataStruct with FuncAmtInvolved
+) extends FuncDataStruct with FuncAmtInvolved {
+  override val funcName = "withdraw"
+}
--- src/main/scala/vsys/events/EventTypes.scala
+++ src/main/scala/vsys/events/EventTypes.scala
@@ -1,45 +1,11 @@
 package vsys.events
 
-import vsys.account.Address
-import vsys.blockchain.transaction.ProcessedTransaction
 import play.api.libs.json.JsObject
 
-trait Event {
-  val url: String
-  val scKey: Option[String]
-  val enKey: Option[String]
-  val maxSize: Int
+case class Event (
+  val url: String,
+  val scKey: Option[String],
+  val enKey: Option[String],
+  val maxSize: Int,
   val subscribeData: JsObject
-}
-
-case class BlockAppendedEvent(
-  override val url: String,
-  override val scKey: Option[String],
-  override val enKey: Option[String],
-  override val maxSize: Int,
-  override val subscribeData: JsObject
-) extends Event
-
-case class TxConfirmedEvent(
-  override val url: String,
-  override val scKey: Option[String],
-  override val enKey: Option[String],
-  override val maxSize: Int,
-  override val subscribeData: JsObject
-) extends Event
-
-case class StateUpdatedEvent(
-  override val url: String,
-  override val scKey: Option[String],
-  override val enKey: Option[String],
-  override val maxSize: Int,
-  override val subscribeData: JsObject
-) extends Event
-
-case class BlockRollbackEvent(
-  override val url: String,
-  override val scKey: Option[String],
-  override val enKey: Option[String],
-  override val maxSize: Int,
-  override val subscribeData: JsObject
-) extends Event
+)
