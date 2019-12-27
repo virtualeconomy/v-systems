208edcae731320b082cd79b9a123174c182e84fd
ncying <ningchen@v.systems>
2019-12-27 10:51:38 +0800

    support contract token balance in API

--- src/main/scala/vsys/api/http/contract/ContractApiRoute.scala
+++ src/main/scala/vsys/api/http/contract/ContractApiRoute.scala
@@ -191,9 +191,10 @@ case class ContractApiRoute (settings: RestAPISettings, wallet: Wallet, utx: Utx
         val height = state.height
         state.tokenInfo(unityKey) match {
           case Some(x) => (for {
-            acc <- Address.fromString(address)
+            acc <- if (Address.fromString(address).isRight) Address.fromString(address)
+                   else ContractAccount.fromString(address)
           } yield Json.obj(
-            "address" -> acc.address,
+            "address/contract" -> address,
             "height" -> height,
             "tokenId" -> tokenIdStr,
             "balance" -> state.tokenAccountBalance(ByteStr(tokenId.arr ++ acc.bytes.arr)),
