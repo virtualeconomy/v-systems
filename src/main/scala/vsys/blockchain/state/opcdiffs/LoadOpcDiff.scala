ce0397ffc98c30605478de8dad246b8985d6b12b
ncying <ningchen@v.systems>
2020-02-21 11:30:32 +0800

    add load transaction id opc

--- src/main/scala/vsys/blockchain/state/opcdiffs/LoadOpcDiff.scala
+++ src/main/scala/vsys/blockchain/state/opcdiffs/LoadOpcDiff.scala
@@ -38,6 +38,15 @@ object LoadOpcDiff extends OpcDiffer {
     }
   }
 
+  def transactionId(context: ExecutionContext)(dataStack: Seq[DataEntry], pointer: Byte): Either[ValidationError, Seq[DataEntry]] = {
+    if (pointer > dataStack.length || pointer < 0) {
+      Left(ContractLocalVariableIndexOutOfRange)
+    } else {
+      val txId = context.transaction.id
+      Right(dataStack.patch(pointer, Seq(DataEntry(txId.arr, DataType.ShortText)), 1))
+    }
+  }
+
   def signerPublicKey(context: ExecutionContext)(dataStack: Seq[DataEntry], pointer: Byte): Either[ValidationError, Seq[DataEntry]] = {
     if (pointer > dataStack.length || pointer < 0) {
       Left(ContractLocalVariableIndexOutOfRange)
@@ -54,11 +63,8 @@ object LoadOpcDiff extends OpcDiffer {
     }
   }
 
-  object LoadType extends Enumeration {
-    val SignerLoad = Value(1)
-    val CallerLoad = Value(2)
-    val TimestampLoad = Value(3)
-    val lastTokenIndexLoad = Value(4)
+  object LoadType extends Enumeration(1) {
+    val SignerLoad, CallerLoad, TimestampLoad, LastTokenIndexLoad, TransactionIdLoad, SignerPublicKeyLoad = Value
   }
 
   override def parseBytesDt(context: ExecutionContext)(bytes: Array[Byte], data: Seq[DataEntry]): Either[ValidationError, Seq[DataEntry]] =
@@ -66,7 +72,9 @@ object LoadOpcDiff extends OpcDiffer {
       case (Some(LoadType.SignerLoad), 2) => signer(context)(data, bytes.last)
       case (Some(LoadType.CallerLoad), 2) => caller(context)(data, bytes.last)
       case (Some(LoadType.TimestampLoad), 2) => timestamp(context)(data, bytes.last)
-      case (Some(LoadType.lastTokenIndexLoad), 2) => lastTokenIndex(context)(data, bytes.last)
+      case (Some(LoadType.LastTokenIndexLoad), 2) => lastTokenIndex(context)(data, bytes.last)
+      case (Some(LoadType.TransactionIdLoad), 2) => transactionId(context)(data, bytes.last)
+      case (Some(LoadType.SignerPublicKeyLoad), 2) => signerPublicKey(context)(data, bytes.last)
       case _ => Left(ContractInvalidOPCData)
     }
 }
