f30bbda69277a1de91f6dd38e30dca10fbdef1e9
ncying <ningchen@v.systems>
2020-06-17 18:31:37 +0800

    change Synchronized map to concurrent hash map

--- src/main/scala/vsys/blockchain/UtxPool.scala
+++ src/main/scala/vsys/blockchain/UtxPool.scala
@@ -1,9 +1,9 @@
 package vsys.blockchain
 
 import java.util.concurrent.locks.ReentrantReadWriteLock
+import java.util.concurrent.ConcurrentHashMap
 
 import cats._
-import com.google.common.cache.CacheBuilder
 import vsys.blockchain.UtxPool.PessimisticPortfolios
 import vsys.settings.{FunctionalitySettings, UtxSettings}
 import vsys.blockchain.state.diffs.TransactionDiffer
@@ -19,8 +19,9 @@ import vsys.blockchain.transaction.ValidationError.GenericError
 import vsys.blockchain.transaction._
 import vsys.utils.{ScorexLogging, Synchronized, Time}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.duration._
-import scala.util.{DynamicVariable, Left, Right}
+import scala.util.{Left, Right}
 
 class UtxPool(time: Time,
               stateReader: StateReader,
@@ -31,16 +32,8 @@ class UtxPool(time: Time,
 
   def synchronizationToken: ReentrantReadWriteLock = new ReentrantReadWriteLock()
 
-  private val transactions = Synchronized(Map.empty[ByteStr, Transaction])
-
-  private lazy val knownTransactions = Synchronized {
-    CacheBuilder
-      .newBuilder()
-      .maximumSize(utxSettings.maxSize * 2)
-      .build[ByteStr, Either[ValidationError, Transaction]]()
-  }
-
-  private val pessimisticPortfolios = Synchronized(new PessimisticPortfolios)
+  private val transactions = new ConcurrentHashMap[ByteStr, Transaction]()
+  private val pessimisticPortfolios = new PessimisticPortfolios
 
   private val sizeStats = Kamon.metrics.histogram("utx-pool-size")
   private val putRequestStats = Kamon.metrics.counter("utx-pool-put-if-new")
@@ -48,72 +41,57 @@ class UtxPool(time: Time,
   private def removeExpired(currentTs: Long): Unit = write { implicit l =>
     def isExpired(tx: Transaction) = (currentTs - tx.timestamp).nanos > utxSettings.maxTransactionAge
 
-    transactions()
-      .values
-      .view
-      .filter(isExpired)
-      .foreach { tx =>
-        transactions.transform(_ - tx.id)
-        pessimisticPortfolios.mutate(_.remove(tx.id))
-      }
+    transactions.values.asScala.filter(isExpired).foreach {
+      tx =>
+        transactions.remove(tx.id)
+        pessimisticPortfolios.remove(tx.id)
+    }
   }
 
-  def putIfNew(tx: Transaction): Either[ValidationError, Boolean] = write { implicit l =>
+  def putIfNew(tx: Transaction): Either[ValidationError, Boolean] = {
     putRequestStats.increment()
-    knownTransactions.mutate(cache =>
-      Option(cache.getIfPresent(tx.id)) match {
-        case Some(Right(_)) => Right(false)
-        case Some(Left(er)) => Left(er)
-        case None =>
-          val res = for {
-            _ <- Either.cond(transactions().size < utxSettings.maxSize, (), GenericError("Transaction pool size limit is reached"))
-            _ <- Either.cond(tx.transactionType != TransactionType.MintingTransaction, (),
-              GenericError("Cannot add MintingTransaction to transaction pool"))
-            _ <- feeCalculator.enoughFee(tx)
-            diff <- TransactionDiffer(fs, history.lastBlock.map(_.timestamp), time.correctedTime(), stateReader.height)(stateReader, tx)
-          } yield {
-            pessimisticPortfolios.mutate(_.add(tx.id, diff))
-            transactions.transform(_.updated(tx.id, tx))
-            tx
-          }
-          cache.put(tx.id, res)
-          sizeStats.record(transactions().size)
-          res.right.map(_ => true)
-      })
+    for {
+      _ <- Either.cond(transactions.size < utxSettings.maxSize, (), GenericError("Transaction pool size limit is reached"))
+      _ <- Either.cond(tx.transactionType != TransactionType.MintingTransaction, (),
+        GenericError("Cannot add MintingTransaction to transaction pool"))
+      _ <- feeCalculator.enoughFee(tx)
+      diff <- TransactionDiffer(fs, history.lastBlock.map(_.timestamp), time.correctedTime(), stateReader.height)(stateReader, tx)
+    } yield {
+      pessimisticPortfolios.add(tx.id, diff)
+      sizeStats.record(transactions.size + 1)
+      Option(transactions.put(tx.id, tx)).isEmpty
+    }
   }
 
   def removeAll(tx: Traversable[Transaction]): Unit = write { implicit l =>
     removeExpired(time.correctedTime())
     tx.view.map(_.id).foreach { id =>
-      knownTransactions.mutate(_.invalidate(id))
-      transactions.transform(_ - id)
-      pessimisticPortfolios.mutate(_.remove(id))
+      transactions.remove(id)
+      pessimisticPortfolios.remove(id)
     }
   }
 
   def portfolio(addr: Address): Portfolio = read { implicit l =>
     val base = stateReader.accountPortfolio(addr)
-    val foundInUtx = pessimisticPortfolios().getAggregated(addr)
+    val foundInUtx = pessimisticPortfolios.getAggregated(addr)
 
     Monoid.combine(base, foundInUtx)
   }
 
   def all(): Seq[Transaction] = read { implicit l =>
-    transactions().values.toSeq.sorted(TransactionsOrdering.InUTXPool)
+    transactions.values.asScala.toSeq.sorted(TransactionsOrdering.InUTXPool)
   }
 
-  def size: Int = read { implicit l => transactions().size }
+  def size: Int = transactions.size
 
-  def transactionById(transactionId: ByteStr): Option[Transaction] = read { implicit l =>
-    transactions().get(transactionId)
-  }
+  def transactionById(transactionId: ByteStr): Option[Transaction] = Option(transactions.get(transactionId))
 
   def packUnconfirmed(): Seq[ProcessedTransaction] = write { implicit l =>
     val currentTs = time.correctedTime()
     removeExpired(currentTs)
     val differ = TransactionDiffer(fs, history.lastBlock.map(_.timestamp), currentTs, stateReader.height) _
-    val (invalidTxs, validTxs, _) = transactions()
-      .values.toSeq
+    val (invalidTxs, validTxs, _) = transactions
+      .values.asScala.toSeq
       .sorted(TransactionsOrdering.InUTXPool)
       .foldLeft((Seq.empty[ByteStr], Seq.empty[ProcessedTransaction], Monoid[Diff].empty)) {
         case ((invalid, valid, diff), tx) if valid.size < Block.MaxTransactionsPerBlockVer1 - 1 =>
@@ -127,10 +105,11 @@ class UtxPool(time: Time,
         case (r, _) => r
       }
 
-    transactions.transform(_ -- invalidTxs)
-    pessimisticPortfolios.mutate { p =>
-      invalidTxs.foreach(p.remove)
+    invalidTxs.foreach { itx =>
+      transactions.remove(itx)
+      pessimisticPortfolios.remove(itx)
     }
+
     validTxs.reverse
   }
 }
@@ -140,8 +119,8 @@ object UtxPool {
   private class PessimisticPortfolios {
     private type Portfolios = Map[Account, Portfolio]
 
-    private val transactionPortfolios = new DynamicVariable(Map.empty[ByteStr, Portfolios])
-    private val transactions = new DynamicVariable(Map.empty[Account, Set[ByteStr]])
+    private val transactionPortfolios = new ConcurrentHashMap[ByteStr, Portfolios]()
+    private val transactions = new ConcurrentHashMap[Account, Set[ByteStr]]()
 
     def add(txId: ByteStr, txDiff: Diff): Unit = {
       val nonEmptyPessimisticPortfolios = txDiff.portfolios
@@ -150,28 +129,30 @@ object UtxPool {
           case (_, portfolio) => portfolio.isEmpty
         }
 
-      if (nonEmptyPessimisticPortfolios.nonEmpty) {
-        transactionPortfolios.value = transactionPortfolios.value + (txId -> nonEmptyPessimisticPortfolios)
+      if (nonEmptyPessimisticPortfolios.nonEmpty &&
+        Option(transactionPortfolios.put(txId, nonEmptyPessimisticPortfolios)).isEmpty) {
         nonEmptyPessimisticPortfolios.keys.foreach { address =>
-          transactions.value = transactions.value + (address -> (transactions.value.getOrElse(address, Set.empty) + txId))
+          transactions.put(address, transactions.getOrDefault(address, Set.empty) + txId)
         }
       }
     }
 
     def getAggregated(accountAddr: Address): Portfolio = {
       val portfolios = for {
-        txIds <- transactions.value.get(accountAddr).toSeq
-        txId <- txIds
-        txPortfolios <- transactionPortfolios.value.get(txId)
-        txAccountPortfolio <- txPortfolios.get(accountAddr)
+        txId <- transactions.getOrDefault(accountAddr, Set.empty).toSeq
+        txPortfolios = transactionPortfolios.getOrDefault(txId, Map.empty[Account, Portfolio])
+        txAccountPortfolio <- txPortfolios.get(accountAddr).toSeq
       } yield txAccountPortfolio
 
       Monoid.combineAll[Portfolio](portfolios)
     }
 
     def remove(txId: ByteStr): Unit = {
-      transactionPortfolios.value = transactionPortfolios.value - txId
-      transactions.value = transactions.value.map { case (k, v) => k -> (v - txId) }
+      if (Option(transactionPortfolios.remove(txId)).isDefined) {
+        transactions.keySet().asScala.foreach { addr =>
+          transactions.put(addr, transactions.getOrDefault(addr, Set.empty) - txId)
+        }
+      }
     }
   }
 
