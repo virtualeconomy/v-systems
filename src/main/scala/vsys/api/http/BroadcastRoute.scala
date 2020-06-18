7d8d2c35a051d42837aecb4cbbc7d0e1e48947ec
ncying <ningchen@v.systems>
2020-06-18 13:47:35 +0800

    add limited broadcast func

--- src/main/scala/vsys/api/http/BroadcastRoute.scala
+++ src/main/scala/vsys/api/http/BroadcastRoute.scala
@@ -1,11 +1,15 @@
 package vsys.api.http
 
 import io.netty.channel.group.ChannelGroup
+import monix.execution.Scheduler
 import vsys.blockchain.transaction.{Transaction, ValidationError}
+import vsys.blockchain.transaction.ValidationError._
 import vsys.blockchain.UtxPool
 import vsys.network._
+import vsys.utils.Schedulers._
 
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 trait BroadcastRoute {
   def utx: UtxPool
@@ -18,6 +22,23 @@ trait BroadcastRoute {
     (for {
       tx <- v
       utxResult <- utx.putIfNew(tx)
+    } yield {
+      if (utxResult) {
+        allChannels.broadcastTx(tx, None)
+      }
+      tx
+    }).left.map(ApiError.fromValidationError)
+
+  }
+
+  protected def doBroadcastWithLimit(v: Either[ValidationError, Transaction], limitedScheduler: Scheduler): Future[Either[ApiError, Transaction]] = Future {
+    (for {
+      tx <- v
+      g = executeCatchingInterruptedException(limitedScheduler)(utx.putIfNew(tx)).recover {
+        case _ =>
+          Left(GenericError("Wait to much time in validation"))
+      }
+      utxResult <- Await.result(g, Duration.Inf)
     } yield {
       if (utxResult) {
         allChannels.broadcastTx(tx, None)
