fb1104b8dffb9053c3e7d3db7d228c965da7430f
zl730 <barde@v.systems>
2020-06-24 17:56:21 +0800

    fix test issues

--- src/test/scala/vsys/blockchain/contract/ContractGenHelper.scala
+++ src/test/scala/vsys/blockchain/contract/ContractGenHelper.scala
@@ -73,8 +73,8 @@ object ContractGenHelper extends TransactionGen {
     Bytes.concat(retType, paraType)
   }
 
-  def dataListGen(seqDataByte: Seq[Array[Byte]], seqDataType: Seq[DataType.Value]): Gen[Seq[DataEntry]] =
-    seqDataByte.zip(seqDataType).map { case (e1: Array[Byte], e2: DataType.Value) => DataEntry.create(e1, e2).explicitGet()}
+  def dataListGen(seqDataByte: Seq[Array[Byte]], seqDataType: Seq[DataType.DataTypeVal]): Gen[Seq[DataEntry]] =
+    seqDataByte.zip(seqDataType).map { case (e1: Array[Byte], e2: DataType.DataTypeVal) => DataEntry.create(e1, e2).explicitGet()}
 
   def basicContractTestGen(): Gen[(PrivateKeyAccount, Long, Long)] = for {
     master <- accountGen
