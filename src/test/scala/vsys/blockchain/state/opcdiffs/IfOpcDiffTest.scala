a5085b5cf17837eeea2d243b89b719146da2e570
Icermli <haohan@excelsia.io>
2020-09-18 14:55:43 +0800

    give else a validation error

--- src/test/scala/vsys/blockchain/state/opcdiffs/IfOpcDiffTest.scala
+++ src/test/scala/vsys/blockchain/state/opcdiffs/IfOpcDiffTest.scala
@@ -66,19 +66,16 @@ class IfOpcDiffTest extends PropSpec with PropertyChecks with GeneratorDrivenPro
       Array[Byte](2.toByte, 0.toByte, 1.toByte, 2.toByte),
       Seq[DataEntry](DataEntry(Array(1.toByte), DataType.Boolean),
         DataEntry(Shorts.toByteArray(0), DataType.OpcBlock),
-        DataEntry(Shorts.toByteArray(0), DataType.OpcBlock))) should be (
+        DataEntry(Shorts.toByteArray(1), DataType.OpcBlock))) should be (
       Right((OpcDiff(Map(),Map(),Map(),Map(),Map(),Map(),Map(),Map()),
         List(DataEntry(Array(1.toByte), DataType.Boolean),
           DataEntry(Shorts.toByteArray(0), DataType.OpcBlock),
-          DataEntry(Shorts.toByteArray(0), DataType.OpcBlock)))))
+          DataEntry(Shorts.toByteArray(1), DataType.OpcBlock)))))
     IfOpcDiff.parseBytes(executionContext)(
       Array[Byte](2.toByte, 0.toByte, 1.toByte, 2.toByte),
       Seq[DataEntry](DataEntry(Array(0.toByte), DataType.Boolean),
         DataEntry(Shorts.toByteArray(0), DataType.OpcBlock),
-        DataEntry(Shorts.toByteArray(0), DataType.OpcBlock))) should be (
-      Right((OpcDiff(Map(),Map(),Map(),Map(),Map(),Map(),Map(),Map()),
-        List(DataEntry(Array(0.toByte), DataType.Boolean),
-          DataEntry(Shorts.toByteArray(0), DataType.OpcBlock),
-          DataEntry(Shorts.toByteArray(0), DataType.OpcBlock)))))
+        DataEntry(Shorts.toByteArray(1), DataType.OpcBlock))) should be (
+      Left(ContractInvalidOPCData))
   }
 }
