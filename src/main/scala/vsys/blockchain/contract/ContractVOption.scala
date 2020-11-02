2b621c913eeab3808dbae343227e6f15195b6e8f
ncying <ningchen@v.systems>
2020-11-02 09:43:10 +0800

    add withdraw trigger

--- src/main/scala/vsys/blockchain/contract/ContractVOption.scala
+++ src/main/scala/vsys/blockchain/contract/ContractVOption.scala
@@ -7,7 +7,7 @@ import vsys.utils.serialization.Deser
 
 object ContractVOption {
   lazy val contract: Contract = Contract.buildContract(Deser.serilizeString("vdds"), Ints.toByteArray(2),
-    Seq(), // Triggers
+    Seq(initTrigger, depositTrigger, withdrawTrigger), // Triggers
     Seq(), // Functions
     stateVarSeq, // StateVars
     stateMapSeq, // StateMaps
@@ -127,6 +127,55 @@ object ContractVOption {
   val depositTextualBytes: Array[Byte] = textualFunc("deposit", Seq(), depositPara)
 
   // WithDraw Trigger
+  val withdrawId: Short = 2
+  val withdrawPara: Seq[String] = Seq("withdrawer", "amount", "tokenId") ++
+                                  Seq("baseTokenId", "targetTokenId", "optionTokenId", "proofTokenId", "isValidTokenId",
+                                      "isBaseToken", "baseTokenIfBlock", "isTargetToken", "targetTokenIfBlock",
+                                      "isOptionToken", "optionTokenIfBlock", "isProofToken", "proofTokenIfBlock")
+  val withdrawDataType: Array[Byte] = Array(DataType.Address.id.toByte, DataType.Amount.id.toByte, DataType.TokenId.id.toByte)
+  val withdrawTriggerOpcs: Seq[Array[Byte]] = Seq(
+    assertCaller ++ Array(0.toByte),
+    cdbvrGet ++ Array(baseTokenIdStateVar.index, 3.toByte),
+    cdbvrGet ++ Array(targetTokenIdStateVar.index, 4.toByte),
+    cdbvrGet ++ Array(optionTokenIdStateVar.index, 5.toByte),
+    cdbvrGet ++ Array(proofTokenIdStateVar.index, 6.toByte),
+    basicConstantGet ++ DataEntry(Array(0.toByte), DataType.Boolean).bytes ++ Array(7.toByte),
+    compareBytesEqual ++ Array(2.toByte, 3.toByte, 8.toByte),
+    basicConstantGet ++ DataEntry(genFunctionOpcs(
+      Seq(
+        cdbvMapValMinus ++ Array(baseTokenBalanceMap.index, 0.toByte, 1.toByte),
+        basicConstantGet ++ DataEntry(Array(1.toByte), DataType.Boolean).bytes ++ Array(7.toByte),
+      )
+    ), DataType.OpcBlock).bytes ++ Array(9.toByte),
+    compareBytesEqual ++ Array(2.toByte, 4.toByte, 10.toByte),
+    basicConstantGet ++ DataEntry(genFunctionOpcs(
+      Seq(
+        cdbvMapValMinus ++ Array(targetTokenBalanceMap.index, 0.toByte, 1.toByte),
+        basicConstantGet ++ DataEntry(Array(1.toByte), DataType.Boolean).bytes ++ Array(7.toByte),
+      )
+    ), DataType.OpcBlock).bytes ++ Array(11.toByte),
+    compareBytesEqual ++ Array(2.toByte, 5.toByte, 12.toByte),
+    basicConstantGet ++ DataEntry(genFunctionOpcs(
+      Seq(
+        cdbvMapValMinus ++ Array(optionTokenBalanceMap.index, 0.toByte, 1.toByte),
+        basicConstantGet ++ DataEntry(Array(1.toByte), DataType.Boolean).bytes ++ Array(7.toByte),
+      )
+    ), DataType.OpcBlock).bytes ++ Array(13.toByte),
+    compareBytesEqual ++ Array(2.toByte, 6.toByte, 14.toByte),
+    basicConstantGet ++ DataEntry(genFunctionOpcs(
+      Seq(
+        cdbvMapValMinus ++ Array(optionTokenBalanceMap.index, 0.toByte, 1.toByte),
+        basicConstantGet ++ DataEntry(Array(1.toByte), DataType.Boolean).bytes ++ Array(7.toByte),
+      )
+    ), DataType.OpcBlock).bytes ++ Array(15.toByte),
+    conditionIf ++ Array(8.toByte, 9.toByte),
+    conditionIf ++ Array(10.toByte, 11.toByte),
+    conditionIf ++ Array(12.toByte, 13.toByte),
+    conditionIf ++ Array(14.toByte, 15.toByte),
+    assertTrue ++ Array(7.toByte)
+  )
+  lazy val withdrawTrigger: Array[Byte] = getFunctionBytes(withdrawId, onWithDrawTriggerType, nonReturnType, withdrawDataType, withdrawTriggerOpcs)
+  val withdrawTextualBytes: Array[Byte] = textualFunc("withdraw", Seq(), withdrawPara)
 
   // Functions
 
