d5aa2acdeb2182128e07f8a755f8265560635b93
hvoid-build-block <hvoid@v.systems>
2021-02-01 11:20:23 +0800

    save

--- src/it/container/start-vsys.sh
+++ src/it/container/start-vsys.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DEFAULT_NET_IP=`ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
-VSYS_NET_IP=`ifconfig eth1 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
+# DEFAULT_NET_IP=`ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
+# VSYS_NET_IP=`ifconfig eth1 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
 
-echo Default: $DEFAULT_NET_IP
-echo Vsys: $VSYS_NET_IP
-echo Options: $VSYS_OPTS
+# echo Default: $DEFAULT_NET_IP
+# echo Vsys: $VSYS_NET_IP
+# echo Options: $VSYS_OPTS
 
 #java -Dvsys.network.declared-address=$VSYS_NET_IP:$VSYS_PORT $VSYS_OPTS -jar /opt/vsys/vsys.jar /opt/vsys/template.conf
-java -Dvsys.network.declared-address=$DEFAULT_NET_IP:9923 $VSYS_OPTS -jar /opt/vsys/vsys.jar /opt/vsys/template.conf
+java $VSYS_OPTS -jar /opt/vsys/vsys.jar /opt/vsys/conf/template.conf
