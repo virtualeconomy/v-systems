87faf377ce94e4b9714880ae17ef3237c0fa8d3b
ncying <ningchen@v.systems>
2021-03-12 14:02:10 +0800

    add state var and state map

--- src/main/scala/vsys/blockchain/contract/ContractTokenV2.scala
+++ src/main/scala/vsys/blockchain/contract/ContractTokenV2.scala
@@ -1,6 +1,7 @@
 package vsys.blockchain.contract
 
 import com.google.common.primitives.Ints
+import vsys.blockchain.contract.ContractGen._
 import vsys.blockchain.state._
 import vsys.utils.serialization.Deser
 
@@ -8,17 +9,27 @@ object ContractTokenV2 {
   lazy val contractTokenWhiteList: Contract = Contract.buildContract(Deser.serilizeString("vdds"), Ints.toByteArray(2),
     Seq(),
     Seq(),
-    Seq(),
-    Seq(),
+    Seq(issuerStateVar.arr, makerStateVar.arr),
+    Seq(listMap.arr),
     Seq()
   ).explicitGet()
 
   lazy val contractTokenBlackList: Contract = Contract.buildContract(Deser.serilizeString("vdds"), Ints.toByteArray(2),
     Seq(),
     Seq(),
-    Seq(),
-    Seq(),
+    Seq(issuerStateVar.arr, makerStateVar.arr),
+    Seq(listMap.arr),
     Seq()
   ).explicitGet()
+
+  // StateVar
+  val issuerStateVar: StateVar  = StateVar(0.toByte, DataType.Address.id.toByte)
+  val makerStateVar: StateVar   = StateVar(1.toByte, DataType.Address.id.toByte)
+  lazy val stateVarTextual: Array[Byte] = ContractPermitted.stateVarTextual
+
+  // State Map
+  val stateMapWhitelist    = List("whitelist", "userAccount", "isInList")
+  val stateMapBlacklist    = List("blacklist", "userAccount", "isInList")
+  val listMap: StateMap    = StateMap(0.toByte, DataType.Account.id.toByte, DataType.Boolean.id.toByte)
 
 }
