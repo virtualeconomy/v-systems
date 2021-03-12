9a9d320eed3a7212acbc5cdd19a2ca51dd400448
ncying <ningchen@v.systems>
2021-03-12 14:05:09 +0800

    add update list func

--- src/main/scala/vsys/blockchain/contract/ContractTokenV2.scala
+++ src/main/scala/vsys/blockchain/contract/ContractTokenV2.scala
@@ -49,5 +49,19 @@ object ContractTokenV2 {
   lazy val destroyFunc: Array[Byte] = ContractPermitted.depositFunc
   val destroyFuncBytes: Array[Byte] = ContractPermitted.depositFuncBytes
 
+  // Update List
+  val updateListId: Short = 3
+  val updateListPara: Seq[String] = Seq("userAccount",
+    "issuer", "valueTrue")
+  val updateListDataType: Array[Byte] = Array(DataType.Account.id.toByte)
+  val updateListOpcs: Seq[Array[Byte]] = Seq(
+    cdbvrGet ++ Array(issuerStateVar.index, 1.toByte),
+    assertCaller ++ Array(1.toByte),
+    basicConstantGet ++ DataEntry(Array(1.toByte), DataType.Boolean).bytes ++ Array(2.toByte),
+    cdbvMapSet ++ Array(listMap.index, 0.toByte, 2.toByte)
+  )
+  lazy val updateListFunc: Array[Byte] = getFunctionBytes(updateListId, publicFuncType, nonReturnType, updateListDataType, updateListOpcs)
+  val updateListFuncBytes: Array[Byte] = textualFunc("updateList", Seq(), updateListPara)
+
 
 }
