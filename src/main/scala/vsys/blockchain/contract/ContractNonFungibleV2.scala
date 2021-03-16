5735b1ac4d2b8ae8bc7c9e5483f391c6a0095ee0
Icermli <haohan@excelsia.io>
2021-03-16 18:46:32 +0800

    add state var and map

--- src/main/scala/vsys/blockchain/contract/ContractNonFungibleV2.scala
+++ src/main/scala/vsys/blockchain/contract/ContractNonFungibleV2.scala
@@ -1,6 +1,7 @@
 package vsys.blockchain.contract
 
 import com.google.common.primitives.Ints
+import vsys.blockchain.contract.ContractGen.{StateMap, StateVar}
 import vsys.blockchain.state._
 import vsys.utils.serialization.Deser
 
@@ -20,4 +21,16 @@ object ContractNonFungibleV2 {
     Seq(),
     Seq()
   ).explicitGet()
+
+  // StateVar
+  val stateVarName = List("issuer", "maker")
+  val issuerStateVar: StateVar = StateVar(0.toByte, DataType.Address.id.toByte)
+  val makerStateVar: StateVar = StateVar(1.toByte, DataType.Address.id.toByte)
+  lazy val stateVarTextual: Array[Byte] = Deser.serializeArrays(stateVarName.map(x => Deser.serilizeString(x)))
+
+  // StateMap
+  val stateMapWhitelist = List("whitelist", "userAccount", "isInList")
+  val stateMapBlacklist = List("blacklist", "userAccount", "isInList")
+  val listMap: StateMap = StateMap(0.toByte, DataType.Account.id.toByte, DataType.Boolean.id.toByte)
+
 }
