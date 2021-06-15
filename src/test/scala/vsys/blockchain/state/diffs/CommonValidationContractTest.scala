21c12a94637713d63ed425286a3e9a8f0c55d1d3
ncying <ningchen@v.systems>
2021-06-15 10:30:35 +0800

    add validation test for exchange contract settings

--- src/test/scala/vsys/blockchain/state/diffs/CommonValidationContractTest.scala
+++ src/test/scala/vsys/blockchain/state/diffs/CommonValidationContractTest.scala
@@ -9,7 +9,7 @@ import vsys.account.ContractAccount.tokenIdFromBytes
 import vsys.blockchain.block.TestBlock
 import vsys.blockchain.contract.{Contract, ContractGenHelper, DataEntry}
 import vsys.blockchain.contract.channel.PaymentChannelContractGen
-import vsys.blockchain.contract.token.TokenContractGen
+import vsys.blockchain.contract.token.{TokenContractGen, TokenContractV2Gen}
 import vsys.blockchain.transaction.contract.RegisterContractTransaction
 import vsys.blockchain.transaction.{GenesisTransaction, PaymentTransaction, TransactionGen}
 import vsys.settings.TestFunctionalitySettings
@@ -20,14 +20,16 @@ class CommonValidationContractTest extends PropSpec
   with Matchers
   with TransactionGen
   with TokenContractGen
-  with PaymentChannelContractGen {
+  with PaymentChannelContractGen
+  with TokenContractV2Gen {
 
   private implicit def noShrink[A]: Shrink[A] = Shrink(_ => Stream.empty)
 
   val tContract: Gen[Contract] = tokenContractGen(true)
   val paymentChannelContract: Gen[Contract] = paymentChannelContractGen()
+  val tContractV2: Gen[Contract] = tokenContractV2Gen(true)
 
-  val preconditionsAndContract: Gen[(GenesisTransaction, PaymentTransaction, RegisterContractTransaction, RegisterContractTransaction)] = for {
+  val preconditionsAndContract: Gen[(GenesisTransaction, PaymentTransaction, RegisterContractTransaction, RegisterContractTransaction, RegisterContractTransaction)] = for {
     (master, ts, fee) <- ContractGenHelper.basicContractTestGen()
     contract <- tContract
     dataStack: Seq[DataEntry] <- initTokenDataStackGen(1L, 1L, "init")
@@ -41,10 +43,12 @@ class CommonValidationContractTest extends PropSpec
     dataForPaymentChannel: Seq[DataEntry] <- initPaymentChannelContractDataStackGen(sysTokenId.arr)
     // Register a payment channel that supports VSYS
     regPaymentChannel <- registerPaymentChannelGen(master, pContract, dataForPaymentChannel, description, fee + 10000000000L, ts + 3)
-  } yield (genesis, transfer, regTokenContract, regPaymentChannel)
+    contractV2 <- tContractV2
+    regTokenContractV2 <- registerTokenGen(master, contractV2, dataStack, description, fee + 10000000000L, ts + 2)
+  } yield (genesis, transfer, regTokenContract, regPaymentChannel, regTokenContractV2)
 
   property("disallows contract related tx before allowed height") {
-    forAll(preconditionsAndContract) { case (genesis, _, regToken: RegisterContractTransaction, _) =>
+    forAll(preconditionsAndContract) { case (genesis, _, regToken: RegisterContractTransaction, _, _) =>
       assertDiffEi(Seq(TestBlock.create(Seq(genesis))), TestBlock.create(Seq(regToken)), TestFunctionalitySettings.ContractDisabled) { blockDiffEi =>
         blockDiffEi should produce("must not appear before height")
       }
@@ -52,9 +56,18 @@ class CommonValidationContractTest extends PropSpec
   }
 
   property("disallows deposit withdraw contract related tx before allowed height") {
-    forAll(preconditionsAndContract) { case (g1, p, regToken: RegisterContractTransaction, regChannel: RegisterContractTransaction) =>
+    forAll(preconditionsAndContract) { case (g1, p, regToken: RegisterContractTransaction, regChannel: RegisterContractTransaction, _) =>
       assertDiffEi(Seq(TestBlock.create(Seq(g1)), TestBlock.create(Seq(p))), TestBlock.create(Seq(regToken, regChannel)), TestFunctionalitySettings.ContractDisabled) { blockDiffEi =>
         blockDiffEi should produce("deposit withdraw contracts must not appear before height")
+      }
+    }
+  }
+
+  property("disallows exchange contract related tx before allowed height") {
+    forAll(preconditionsAndContract) { case (g1, p, regToken: RegisterContractTransaction, regChannel: RegisterContractTransaction, regTokenV2: RegisterContractTransaction) =>
+      assertDiffEi(Seq(TestBlock.create(Seq(g1)), TestBlock.create(Seq(p)), TestBlock.create(Seq(regToken))),
+        TestBlock.create(Seq(regChannel, regTokenV2)), TestFunctionalitySettings.ContractDisabled) { blockDiffEi =>
+        blockDiffEi should produce("exchange contracts must not appear before height")
       }
     }
   }
